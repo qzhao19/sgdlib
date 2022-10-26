19468f74da5caf1b9d240527cd261f2edf615d07
qzhao19 <qi.zhao@outlook.fr>
2022-10-26 23:24:44 +0800

    update line search method

--- sgdlib/line_search/backtracking.py
+++ sgdlib/line_search/backtracking.py
@@ -10,4 +10,19 @@ class LineSearchBacktracking(object):
         if step <= 0:
             raise ValueError("'step' must be positive")
         
+        fx0_init = fx0
+
+        # Compute the initial gradient in the search direction
+        dg_init = np.dot(g, d)
+
+        if dg_init > 0:
+            raise RuntimeError("Moving direction increases the objective function value")
         
+        test_decrease_factor = line_search_params.ftol * dg_init
+        width = 0
+
+        iter = 0
+        for iter in range(line_search_params.max_linesearch):
+            x = xp + step * d
+
+            fx0 = self.loss.evaluate(x, g) 
