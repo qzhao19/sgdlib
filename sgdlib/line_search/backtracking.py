49e3239372a94fa0fc82c042ce252ae3b587f890
qzhao19 <qi.zhao@outlook.fr>
2022-11-14 23:24:14 +0800

    update

--- sgdlib/line_search/backtracking.py
+++ sgdlib/line_search/backtracking.py
@@ -1,20 +1,20 @@
 import numpy as np
 
 class LineSearchBacktracking(object):
-    def __init__(self, X, y, loss_fn, linesearch_params):
+    def __init__(self, X, y, loss_func, linesearch_params):
         self.X = X
         self.y = y
-        self.loss_fn = loss_fn
+        self.loss_func = loss_func
         self.linesearch_params = linesearch_params
 
-    def search(self, x, fx0, g, step, d, xp, gp):
-
+    def search(self, x, fx0, g,  d, step, xp):
         # Decreasing and increasing factors
         decrease_factor = self.linesearch_params.decrease_factor
         increase_factor = self.linesearch_params.increase_factor
 
         if step <= 0:
-            raise ValueError("'step' must be positive")
+            print("'step' must be positive")
+            return -1
         
         fx0_init = fx0
 
@@ -22,25 +22,31 @@ class LineSearchBacktracking(object):
         dg_init = np.dot(g, d)
 
         if dg_init > 0:
-            raise RuntimeError("Moving direction increases the objective function value")
+            print("Moving direction increases the objective function value")
+            return -1
         
         dg_test = self.linesearch_params.ftol * dg_init
         width = None
         dg = None
-        iter = 0
-        for iter in range(self.linesearch_params.max_linesearch):
+        count = 0
+        for _ in range(self.linesearch_params.max_linesearch):
+            
+            # x_{k+1} = x_k + step * d_k
             x = xp + step * d
 
             # fx0 = self.loss_fn.evaluate(x, g) 
             fx0 = self.loss_func.evaluate(self.X, self.y, x)
             g = self.loss_func.gradient(self.X, self.y, x)
 
+            # increment count
+            count += 1
+
             if fx0 > fx0_init + step * dg_test:
                 width = decrease_factor
             else:
                 # Armijo condition
                 if self.linesearch_params.condition == "LINESEARCH_BACKTRACKING_ARMIJO":
-                    break
+                    return count
 
                 # compute the project of d on the direction d
                 dg = np.dot(g, d)
@@ -49,19 +55,24 @@ class LineSearchBacktracking(object):
                 else:
                     # check wolf condition
                     if self.linesearch_params.condition == "LINESEARCH_BACKTRACKING_WOLFE":
-                        break
+                        return count
                     if dg > -self.linesearch_params.wolfe * dg_init:
                         width = decrease_factor
                     else:
-                        break
+                        return count
 
             if step < self.linesearch_params.min_step:
-                raise ValueError("the line search step became smaller than the minimum value allowed")
+                print("the line search step became smaller than the minimum value allowed")
+                return -1
 
             if step > self.linesearch_params.max_step:
-                raise ValueError("the line search step became larger than the maximum value allowed")
-
+                print("the line search step became larger than the maximum value allowed")
+                return -1
+        
+            if count >= self.linesearch_params.max_linesearch:
+                print("the line search step reached the max number of iterations")
+                return -1
+            
             step *= width
         
-        if iter >= self.linesearch_params.max_linesearch:
-            raise ValueError("the line search step reached the max number of iterations")
+        return count, x, fx0, g, d, step
