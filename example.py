506a9bb04ea4412a238358e30106c2aea3792019
qzhao19 <qi.zhao@outlook.fr>
2022-11-18 23:54:59 +0800

    add examples case

--- example.py
+++ example.py
@@ -1,19 +1,67 @@
 
 from sgdlib import sgd
+from sgdlib import sag
+from sgdlib import scd
+
+from sgdlib import lbfgs
 from sgdlib.loss import log_loss
 from sgdlib.utils import load_data
 from sgdlib.utils import Regularizer, StepDecay
 
-def main():
-    X, y = load_data("./data/ionosphere.data", sep = ",")
 
-    optimizer = sgd.SGD(loss = log_loss.LogLoss(), 
+def test_sgd(X, y):
+    optimizer = sgd.SGD(
+        loss_func = log_loss.LogLoss(), 
         lr_decay = StepDecay(lr = 0.1), 
         regularizer = Regularizer(penalty = None, alpha = 0.0)
     )
     opt_w = optimizer.optimize(X, y)
+    return opt_w
 
-    print(opt_w)
+def test_sag(X, y):
+    optimizer = sag.SAG(
+        loss_func = log_loss.LogLoss(), 
+        lr_decay = StepDecay(lr = 0.1), 
+        regularizer = Regularizer(penalty = None, alpha = 0.0)
+    )
+    opt_w = optimizer.optimize(X, y)
+    return opt_w
+
+def test_scd(X, y):
+    optimizer = scd.SCD(
+        loss_func = log_loss.LogLoss()
+    )
+    opt_w = optimizer.optimize(X, y)
+    return opt_w
+
+
+def test_lbfgs(X, y):
+    linesearch_params = {
+        "decrease_factor": 0.5, 
+        "increase_factor": 2.1,
+        "ftol": 1e-4,
+        "max_linesearch": 40,
+        "condition": "LINESEARCH_BACKTRACKING_WOLFE",
+        "wolfe": 0.9,
+        "max_step": 1e+20,
+        "min_step": 1e-20
+    }
+    optimizer = lbfgs.LBFGS(
+        linesearch_params = linesearch_params,
+        loss_func = log_loss.LogLoss()
+    )
+
+
+def main():
+    X, y = load_data("./data/ionosphere.data", sep = ",")
+
+    # optimizer = sgd.SGD(loss = log_loss.LogLoss(), 
+    #     lr_decay = StepDecay(lr = 0.1), 
+    #     regularizer = Regularizer(penalty = None, alpha = 0.0)
+    # )
+    # opt_w = optimizer.optimize(X, y)
+
+    # print(opt_w)
 
 if __name__ == '__main__':
     main()
