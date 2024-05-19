2063eb32b5a27a4b942e95dd3ed6d78da470d80f
qzhao19 <qi.zhao@outlook.fr>
2024-05-19 23:48:05 +0800

    create prereqs.hpp

---
+++ sgdlib/common/prereqs.hpp
@@ -0,0 +1,28 @@
+#ifndef COMMON_PREREQS_HPP_
+#define COMMON_PREREQS_HPP_
+
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <random>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <stdexcept>
+#include <utility>
+#include <unordered_set>
+#include <vector>
+
+#include <cxxabi.h>
+
+using FeatureType = double;
+using LabelType = long;
+
+#endif // COMMON_PREREQS_HPP_
