d7b9bc757e9a79443603f7092eed0fbbf64b89e5
qzhao19 <qi.zhao@outlook.fr>
2024-05-23 23:35:34 +0800

    update

--- tests/test_registry.cpp
+++ tests/test_registry.cpp
@@ -10,9 +10,8 @@ namespace sgdlib {
 
 class Foo {
 public:
-    explicit Foo(int x) { 
-        std::cout << "Foo" <<std::endl; 
-    }
+    explicit Foo(int x) {}
+    virtual void Say() = 0;
 };
 
 DECLARE_REGISTRY(FooRegistry, Foo, int);
@@ -20,22 +19,28 @@ DEFINE_REGISTRY(FooRegistry, Foo, int);
 
 class Bar : public Foo {
 public:
-    explicit Bar(int x) : Foo(x) { 
-        std::cout << "Bar" <<std::endl;  
+    explicit Bar(int x) : Foo(x) {}
+
+    virtual void Say() {
+        std::cout << "I am Bar" << std::endl;
     }
 };
 REGISTER_CLASS(FooRegistry, Bar, Bar);
 
 class AnotherBar : public Foo {
 public:
-    explicit AnotherBar(int x) : Foo(x) { 
-        std::cout << "Another Bar" <<std::endl;  
+    explicit AnotherBar(int x) : Foo(x) { }
+
+    virtual void Say() {
+        std::cout << "I am Another Bar" << std::endl;
     }
 };
 REGISTER_CLASS(FooRegistry, AnotherBar, AnotherBar);
 
 TEST(TestRegistry, CanRunCreator) {
-    std::unique_ptr<Foo> bar(FooRegistry()->Create("Bar", 1));
+    std::unique_ptr<Foo> bar = FooRegistry()->Create("Bar", 1);
+    bar->Say();
+
     EXPECT_TRUE(bar != nullptr) << "Cannot create bar";
     std::unique_ptr<Foo> another_bar(FooRegistry()->Create("AnotherBar", 1));
     EXPECT_TRUE(another_bar != nullptr);
