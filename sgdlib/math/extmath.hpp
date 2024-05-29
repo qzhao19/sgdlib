6280ff46a419ea5ecac3faa1ae6f8cc6584d195e
qzhao19 <qi.zhao@outlook.fr>
2024-05-29 23:16:17 +0800

    update

--- sgdlib/math/extmath.hpp
+++ sgdlib/math/extmath.hpp
@@ -15,6 +15,19 @@ Type sigmoid(Type x) {
     return 1 / (1 + std::exp(-x));
 };
 
+/**
+ * @brief Clip (limit) the values in a vector.
+*/
+template<typename Type>
+void clip(std::vector<Type>& x, Type min, Type max) {
+    std::transform(std::begin(x), std::end(x), std::begin(x),
+        [=] (auto i) { 
+            return std::clamp(i, min, max); 
+        }
+    );
+}
+
+
 } // namespace internal
 } // namespace sgdlib
 
