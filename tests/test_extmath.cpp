8a799e1c81432a18ab44f8e5ac80409b0ecf5fda
qzhao19 <qi.zhao@outlook.fr>
2024-05-30 23:34:30 +0800

    update

--- tests/test_extmath.cpp
+++ tests/test_extmath.cpp
@@ -10,6 +10,15 @@ TEST(ExtmathTest, SigmoidTest){
     std::cout << x << std::endl;
     EXPECT_DOUBLE_EQ(x, 0.9999546021312976);
 };
- 
+
+TEST(ExtmathTest, ClipTest){
+    std::vector<double> x = {5.2, 3.3, 1.2, 0.3, 
+                             4.8, 3.1, 1.6, 0.2};
+    sgdlib::internal::clip<double>(x, 2.0, 4.0);
+    std::vector<double> expect = {4.0, 3.3, 2.0, 2.0,
+                                  4.0, 3.1, 2.0, 2.0};
+    EXPECT_EQ(x, expect);
+};
+
 }
 
