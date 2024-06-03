349dc7d0f1d97a971a45bdd40ebf18e18f04d5d0
qzhao19 <qi.zhao@outlook.fr>
2024-06-03 23:04:28 +0800

    update

--- tests/test_registry.cpp
+++ tests/test_registry.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-#include <vector>
-
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
 #include "sgdlib/common/registry.hpp"
-#include "sgdlib/core/loss/log_loss.hpp"
 
 namespace sgdlib {
 
@@ -49,35 +46,6 @@ TEST(TestRegistry, CreatorForFoo) {
     EXPECT_TRUE(another_bar != nullptr);
 }
 
-
-TEST(TestRegistry, CreatorForLogLoss) {
-    std::vector<double> X = {5.2, 3.3, 1.2, 0.3,
-                            4.8, 3.1 , 1.6, 0.2,
-                            4.75, 3.1, 1.32, 0.1,
-                            5.9, 2.6, 4.1, 1.2,
-                            5.1, 2.2, 3.3, 1.1,
-                            5.2, 2.7, 4.1, 1.3,
-                            6.6, 3.1, 5.25, 2.2,
-                            6.3, 2.5, 5.1, 2.0,
-                            6.5, 3.1, 5.2, 2.1};
-    std::vector<long> y = {0, 0, 0, 0, 1, 1, 1, 1, 1};
-    std::vector<double> w = {0.9781, 0.9711, 0.3962, 0.5209};
-
-    LossParamType loss_param = {{"alpha", 0.0}};
-    std::unique_ptr<sgdlib::LossFunction> loss_fn = LossFunctionRegistry()->Create("LogLoss", loss_param);
-    double loss = loss_fn->evaluate(X, y, w);
-    std::cout << loss << std::endl;
-
-    EXPECT_DOUBLE_EQ(loss, 4.0159203584860403);
-
-    std::vector<double> grad(4);
-    loss_fn->gradient(X, y, w, grad);
-
-    std::vector<double> expect ={2.2939883890082928, 1.3441739235904036, 
-                                 0.9131519457304953, 0.19995927206507597};
-    EXPECT_EQ(grad, expect);
-
-}
 
 } // end of namespace
 
