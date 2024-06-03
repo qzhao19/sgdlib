b2a1287d87443984714e34fef6fee527ba6c04a0
qzhao19 <qi.zhao@outlook.fr>
2024-06-03 23:04:21 +0800

    update

--- tests/test_lr_decay.cpp
+++ tests/test_lr_decay.cpp
@@ -3,9 +3,8 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-#include "sgdlib/core/decay_policy/base.hpp"
-#include "sgdlib/core/decay_policy/exponential_decay.hpp"
-#include "sgdlib/core/decay_policy/invscaling_decay.hpp"
+#include "sgdlib/core/lr_decay.hpp"
+
 
 namespace sgdlib {
 
@@ -13,34 +12,25 @@ class LRDecayTest : public ::testing::Test {
 public:
     virtual void SetUp(std::string lr_decay_policy) {
         double eta0 = 0.0001, decay = 0.0025;
-
-        if (lr_decay_policy == "invscaling") {
-            lr_decay = new sgdlib::internal::InvscalingDecay(eta0, decay);
-        }
-        else if (lr_decay_policy == "exponential") {
-            lr_decay = new sgdlib::internal::ExponentialDecay(eta0, decay);
-        }
+        lr_decay = LRDecayRegistry()->Create(lr_decay_policy, eta0, decay);
     }
 
-    virtual void TearDown() {
-        if (lr_decay) {
-            delete lr_decay;
-            lr_decay = nullptr;
-        }
-    }
-    sgdlib::internal::LRDecay* lr_decay;
+    virtual void TearDown() {}
+    std::unique_ptr<sgdlib::LRDecay> lr_decay; 
 };
 
 TEST_F(LRDecayTest, InvscalingDecayTest) {
-    SetUp("invscaling");
+    SetUp("Invscaling");
     double eta = lr_decay->compute(10);
     std::cout << eta << std::endl;
+    EXPECT_DOUBLE_EQ(eta, 9.9426007395295659e-05);
 }
 
 TEST_F(LRDecayTest, ExponentialDecayTest) {
-    SetUp("exponential");
+    SetUp("Exponential");
     double eta = lr_decay->compute(10);
     std::cout << eta << std::endl;
+    EXPECT_DOUBLE_EQ(eta, 9.7530991202833268e-05);
 }
 
 }
