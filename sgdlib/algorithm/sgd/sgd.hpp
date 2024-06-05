88d5db911c629cca6b72b9a09e1ef0e0b1a75cff
qzhao19 <qi.zhao@outlook.fr>
2024-06-05 23:07:05 +0800

    update

--- sgdlib/algorithm/sgd/sgd.hpp
+++ sgdlib/algorithm/sgd/sgd.hpp
@@ -4,10 +4,8 @@
 #include "common/prereqs.hpp"
 #include "common/predefs.hpp"
 #include "algorithm/base.hpp"
-#include "core/loss/base.hpp"
-#include "core/decay_policy/base.hpp"
-#include "core/decay_policy/invscaling_decay.hpp"
-#include "core/decay_policy/exponential_decay.hpp"
+#include "core/loss.hpp"
+#include "core/lr_decay.hpp"
 
 namespace sgdlib {
 
@@ -19,13 +17,9 @@ namespace sgdlib {
  * 
 */
 class SGD: public BaseOptimizer {
-portected:
-    std::size_t num_features_;
-
 public:
     SGD(const std::vector<FeatureType>& x0, 
         std::string loss, 
-        std::string penalty,
         std::string lr_policy,
         double alpha,
         double eta0,
@@ -35,20 +29,15 @@ public:
         std::size_t batch_size,
         std::size_t num_iters_no_change,
         std::size_t random_seed,
-        bool fit_intercept = true,
-        bool multi_class = false,
         bool shuffle = true, 
         bool verbose = true): BaseOptimizer(x0, 
-            loss, penalty, 
-            lr_policy, alpha, 
-            eta0, tol, 
-            decay,
+            loss, lr_policy, 
+            alpha, eta0, 
+            tol, decay,
             max_iters, 
             batch_size, 
             num_iters_no_change,
             random_seed,
-            fit_intercept,
-            multi_class,
             shuffle, 
             verbose) {};
     ~SGD () {};
@@ -57,20 +46,14 @@ public:
                   const std::vector<LabelType>& y) override {
         
         std::size_t num_samples = y.size();
-        std::size_t num_features = X.size() / y.size();
-
-        // std::size_t update_num_features;
-        if (this->fit_intercept_) {
-            num_features_ = num_features + 1;
-        }
-        else {
-            num_features_ = num_features;
-        }
+        std::size_t num_features = this->x0_.size();
 
         std::size_t step_per_iter = num_samples / this->batch_size_;
         std::size_t no_improvement_count = 0;
+        std::size_t iter = 0;
 
         bool is_converged = false;
+        bool is_infinity = false;
         double best_loss = INF;
 
         // initialize a lookup table for training X, y
@@ -80,53 +63,48 @@ public:
         // initialize loss, loss_history, gradient, x0 (weight)
         double loss;
         std::vector<double> loss_history(step_per_iter, 0);
-        std::vector<FeatureType> grad(num_features_, 0.0);
-        std::vector<FeatureType> x0(num_features_, 1.0);
-        std::copy(this->x0_.begin(), this->x0_.end(), x0.begin());
+        std::vector<FeatureType> grad(num_features, 0.0);
+        std::vector<FeatureType> x0 = this->x0_;
 
         // initialize X_batch, y_batch and batch_data_index
-        std::vector<FeatureType> X_batch(num_features_*this->batch_size_, 1.0);
-        std::vector<FeatureType> y_batch(this->batch_size_, 0);
+        std::vector<FeatureType> X_batch(num_features*this->batch_size_);
+        std::vector<LabelType> y_batch(this->batch_size_);
         std::vector<std::size_t> batch_data_index(this->batch_size_);
         
         // initialize loss function 
-        std::unique_ptr<sgdlib::LossFunction> loss_fn = LossFunctionRegistry()->Create(loss, 0.0);
+        std::unique_ptr<sgdlib::LossFunction> loss_fn = LossFunctionRegistry()->Create(
+            this->loss_, this->loss_params_
+        );
 
         // initialize learning rate scheduler
-        if (lr_policy == "invscaling") {
-            lr_decay = std::make_shared<sgdlib::internal::InvscalingDecay>(
-                this->eta0_, this->decay_
-            );
-        }
-        else if (lr_policy == "exponential") {
-            lr_decay = std::make_shared<sgdlib::internal::ExponentialDecay>(
-                this->eta0_, this->decay_
-            );
-        }
+        std::unique_ptr<sgdlib::LRDecay> lr_decay = LRDecayRegistry()->Create(
+            this->lr_policy_, this->eta0_, this->decay_
+        );
 
         // 
-        for (std::size_t iter = 0; iter < this->max_iters_; iter++) {
+        for (iter = 0; iter < this->max_iters_; ++iter) {
             // enable to shuffle mask of data for on batch
             if (this->shuffle_) {
-                this->random_state_.shuffle<FeatureType>(X_data_index);
+                this->random_state_.shuffle<std::size_t>(X_data_index);
             }
 
             // apply lr decay policy to compute eta
             double eta = lr_decay->compute(iter);
             for (std::size_t i = 0; i < step_per_iter; ++i) {
                 // copy batch data indices to batch_data_index
-                std::copy(&X_data_index[i] * this->batch_size_, 
-                          (&X_data_index[i] + 1) * this->batch_size_, 
-                          batch_data_index);
+                std::copy_n(X_data_index.begin() + (i * this->batch_size_), 
+                            this->batch_size_, 
+                            std::back_inserter(batch_data_index));
                 
                 // copy X_batch and y_batch data
                 for (std::size_t j = 0; j < this->batch_size_; ++j) {
                     std::copy(&X[batch_data_index[j] * num_features], 
                               &X[(batch_data_index[j] + 1) * num_features], 
-                              X_batch.begin() + (j * num_features_));
+                              X_batch.begin() + (j * num_features));
                     y_batch[j] = y[batch_data_index[j]];
                 };
 
+                // evaluate the loss on X_batch
                 loss = loss_fn->evaluate(X_batch, y_batch, x0);
 
                 loss_fn->gradient(X_batch, y_batch, x0, grad);
@@ -135,7 +113,7 @@ public:
                 sgdlib::internal::clip<FeatureType>(grad, MIN_DLOSS, MAX_DLOSS);
 
                 // update x0: w = w - lr * grad
-                for (std::size_t k = 0; k < num_features_; ++k) {
+                for (std::size_t k = 0; k < num_features; ++k) {
                     x0[k] -= eta * grad[k]; 
                 }
 
@@ -143,28 +121,58 @@ public:
                 loss_history[i] = loss;
             }
 
-            double sum_loss = std::accumulate(loss_history.begin(), loss_history.end(), 0);
-            // std::fill(loss_history.begin(), loss_history.end(), 0);
+            // ---Convergence test---
+            // check under/overflow
+            if (sgdlib::internal::isinf(x0)) {
+                is_infinity = true;
+                break;
+            }
 
-            if (sum_loss > best_loss - this->tol_ * this->batch_size_) {
+            // evaluate the loss on the training dataset
+            double sum_loss = std::accumulate(loss_history.begin(), loss_history.end(), 0);
+            if ((this->tol_ > -INF) && (sum_loss > best_loss - this->tol_ * this->batch_size_)) {
                 no_improvement_count +=1;
             }
             else {
                 no_improvement_count = 0;
             }
-
             if (sum_loss < best_loss) {
                 best_loss = sum_loss;
             }
 
+            // 
             if (no_improvement_count >= this->num_iters_no_change_) {
+                if (this->verbose_) {
+                    std::cout << "Convergence after " << (iter + 1) << " epochs." << std::endl;
+                }
                 is_converged = true;
-                this->opt_x_ = x0;
                 break;
             }
 
-
+            if (this->verbose_) {
+                if ((iter % 1) == 0) {
+                    std::cout << "-- Epoch = " << iter << ", average loss value = " 
+                              << sum_loss / static_cast<double>(this->batch_size_) << std::endl;
+                }
+            }
         }
+
+        if (is_infinity) {
+            std::ostringstream err_msg;
+            err_msg << "Floating-point under-/overflow occurred at epoch " << (iter + 1)
+                    << ", try to scale input data with standard or minmax." << std::endl;
+            throw std::runtime_error(err_msg.str());
+        }
+
+        if (!is_converged) {
+            std::ostringstream err_msg;
+            err_msg << "Not converge, current number of epoch = " << (iter + 1)
+                    << ", the batch size = " << this->batch_size_ 
+                    << ", try apply different parameters." << std::endl;
+            throw std::runtime_error(err_msg.str());
+        }
+
+        this->x_opt_ = x0;
     }
 
 };
