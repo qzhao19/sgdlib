423b01b63093a0ec7f3660923aad63d5e116946a
qzhao19 <qi.zhao@outlook.fr>
2024-06-05 23:06:58 +0800

    update

--- sgdlib/algorithm/base.hpp
+++ sgdlib/algorithm/base.hpp
@@ -2,18 +2,20 @@
 #define ALGORITHM_BASE_HPP_
 
 #include "common/prereqs.hpp"
+#include "common/predefs.hpp"
 #include "math/random.hpp"
 
 namespace sgdlib {
 
 class BaseOptimizer {
-private:
+protected:
     std::vector<FeatureType> x0_;
     std::string loss_;
     std::string penalty_;
     std::string lr_policy_;
 
     double alpha_;
+    double l1_ratio_;
     double eta0_;
     double tol_;
     double decay_;
@@ -23,19 +25,17 @@ private:
     std::size_t num_iters_no_change_;
     std::size_t random_seed_;
 
-    bool fit_intercept_;
-    bool multi_class_;
     bool shuffle_;
     bool verbose_;
 
-protected:
+    LossParamType loss_params_;
+    std::vector<FeatureType> x_opt_;
     sgdlib::internal::RandomState random_state_;
 
 public:
     BaseOptimizer() {};
     BaseOptimizer(const std::vector<FeatureType>& x0, 
                   std::string loss, 
-                  std::string penalty,
                   std::string lr_policy,
                   double alpha,
                   double eta0,
@@ -45,12 +45,9 @@ public:
                   std::size_t batch_size,
                   std::size_t num_iters_no_change,
                   std::size_t random_seed,
-                  bool fit_intercept,
-                  bool multi_class,
                   bool shuffle = true, 
                   bool verbose = true): x0_(x0), 
             loss_(loss), 
-            penalty_(penalty),
             lr_policy_(lr_policy),
             alpha_(alpha),
             eta0_(eta0),
@@ -60,22 +57,25 @@ public:
             batch_size_(batch_size),
             num_iters_no_change_(num_iters_no_change),
             random_seed_(random_seed),
-            fit_intercept_(fit_intercept),
-            multi_class_(multi_class),
             shuffle_(shuffle),
             verbose_(verbose) {
         if (random_seed_ == -1) {
-            random_state_ = decisiontree::RandomState();
+            random_state_ = sgdlib::internal::RandomState();
         }
         else {
-            random_state_ = decisiontree::RandomState(random_seed_);
+            random_state_ = sgdlib::internal::RandomState(random_seed_);
         }
+        loss_params_["alpha"] = alpha;
     };
     
     ~BaseOptimizer() {};
 
     virtual void optimize(const std::vector<FeatureType>& X, 
                           const std::vector<LabelType>& y) = 0;
+
+    const std::vector<FeatureType> get_coef() const {
+        return x_opt_;
+    }
 
 };
 
