5f7db8caa04096c18edeabaea55157a592184ddd
qzhao19 <qi.zhao@outlook.fr>
2024-06-13 21:54:35 +0800

    add sag.hpp

---
+++ sgdlib/algorithm/sgd/sag.hpp
@@ -0,0 +1,57 @@
+#ifndef ALGORITHM_SGD_SAG_HPP_
+#define ALGORITHM_SGD_SAG_HPP_
+
+#include "algorithm/base.hpp"
+
+namespace sgdlib {
+
+/**
+ * @file sag.hpp
+ * 
+ * @brief @brief Stochastic Average Gradient Descent (SAGD) optimizer.
+ * 
+ * 
+*/
+class SAG: public BaseOptimizer {
+protected:
+    void lagged_update() {
+        
+    }
+
+public:
+    SAG(const std::vector<FeatureType>& x0, 
+        std::string loss, 
+        std::string lr_policy,
+        double alpha,
+        double eta0,
+        double tol,
+        double gamma,
+        std::size_t max_iters, 
+        std::size_t batch_size,
+        std::size_t num_iters_no_change,
+        std::size_t random_seed,
+        bool shuffle = true, 
+        bool verbose = true): BaseOptimizer(x0, 
+            loss, lr_policy, 
+            alpha, eta0, 
+            tol, gamma,
+            max_iters, 
+            batch_size, 
+            num_iters_no_change,
+            random_seed,
+            shuffle, 
+            verbose) {};
+    ~SAG() {};
+
+    void optimize(const std::vector<FeatureType>& X, 
+                  const std::vector<LabelType>& y) override {
+
+        
+    }
+
+
+}
+
+} // namespace sgdlib
+
+#endif // ALGORITHM_SGD_SAG_HPP_
