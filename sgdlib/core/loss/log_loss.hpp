0ff9303ba87568d958ab206623027602a5bf6e5d
qzhao19 <qi.zhao@outlook.fr>
2024-06-20 23:06:15 +0800

    update

--- sgdlib/core/loss/log_loss.hpp
+++ sgdlib/core/loss/log_loss.hpp
@@ -23,15 +23,19 @@ public:
     */
     double evaluate(const std::vector<FeatureType>& X, 
                     const std::vector<LabelType>& y, 
-                    const std::vector<FeatureType>& w) const override {
+                    const std::vector<FeatureType>& weight, 
+                    const FeatureType& bias) const override {
+    
         std::size_t num_samples = y.size();
-        std::size_t num_features = w.size();  
+        std::size_t num_features = weight.size();  
         double loss = 0.0;
         double X_w, y_hat;
         for (std::size_t i = 0; i < num_samples; ++i) {
             X_w = std::inner_product(X.begin() + (i * num_features), 
                                      X.begin() + ((i + 1) * num_features), 
-                                     w.begin(), 0.0);      
+                                     weight.begin(), 0.0);
+            X_w *= loss_param_.at("wscale");
+            X_w += bias; 
             y_hat = sgdlib::internal::sigmoid<FeatureType>(X_w);
 
             loss -= (static_cast<double>(y[i]) * std::log(y_hat) + 
@@ -39,12 +43,11 @@ public:
         }
         loss /= static_cast<double>(num_samples);
         
-        // calculate 
-        double reg = std::inner_product(w.begin(), 
-                                        w.end(), 
-                                        w.begin(), 0.0);
+        // calculate regulization term
+        double reg = std::inner_product(weight.begin(), 
+                                        weight.end(), 
+                                        weight.begin(), 0.0);
         reg /= static_cast<double>(num_samples);
-        
         return loss + reg * loss_param_.at("alpha");
     }
 
@@ -52,7 +55,7 @@ public:
      * Evaluate the gradient of logistic regression log-likelihood
      * function with the given parameters using the given batch 
      * size from the given point label
-     *    - sum(x_i * (s(w_T*x_i) - y_i))
+     *    - (1/N) * sum(x_i * (s(w_T*x_i) - y_i))
      * 
      * @param[in] X 1darray of shape (num_samples * num_features), the matrix of input data
      * @param[in] y 1darray of shape (num_samples) 
@@ -60,30 +63,37 @@ public:
     */
     void gradient(const std::vector<FeatureType>& X, 
                   const std::vector<LabelType>& y, 
-                  const std::vector<FeatureType>& w,
-                  std::vector<FeatureType>& grad) const override {
+                  const std::vector<FeatureType>& weight,
+                  const FeatureType& bias,
+                  std::vector<FeatureType>& weight_grad, 
+                  FeatureType& bias_grad) const override {
+    
         std::size_t num_samples = y.size();
-        std::size_t num_features = w.size();
+        std::size_t num_features = weight.size();
         double X_w;
         std::vector<FeatureType> y_hat(num_samples, 0.0);
 
         for (std::size_t i = 0; i < num_samples; ++i) {
             X_w = std::inner_product(X.begin() + (i * num_features), 
                                      X.begin() + ((i + 1) * num_features), 
-                                     w.begin(), 0.0);
+                                     weight.begin(), 0.0);
+            X_w *= loss_param_.at("wscale");
+            X_w += bias;
             y_hat[i] = sgdlib::internal::sigmoid<FeatureType>(X_w);
+            bias_grad += (y_hat[i] - static_cast<FeatureType>(y[i]));
         }
+        bias_grad /= static_cast<FeatureType>(num_samples);
 
         FeatureType inner_prod = 0.0;
-        std::size_t fx_index = 0;
-        while (fx_index < num_features) {
+        std::size_t feature_index = 0;
+        while (feature_index < num_features) {
             for (std::size_t i = 0; i < num_samples; ++i) {
-                inner_prod += (X[i * num_features + fx_index]) * 
+                inner_prod += (X[i * num_features + feature_index]) * 
                               (y_hat[i] - static_cast<FeatureType>(y[i]));
             }
-            grad[fx_index] = inner_prod / static_cast<FeatureType>(num_samples) + 
-                             loss_param_.at("alpha") * 2.0 * w[fx_index];
-            ++fx_index;
+            weight_grad[feature_index] = inner_prod / static_cast<FeatureType>(num_samples) + 
+                                         loss_param_.at("alpha") * 2.0 * weight[feature_index];
+            ++feature_index;
             inner_prod = 0.0;
         }
     }
