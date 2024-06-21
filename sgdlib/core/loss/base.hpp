a6dd82e42af189ed13cb72402c26ae936d5358a1
qzhao19 <qi.zhao@outlook.fr>
2024-06-21 23:48:19 +0800

    update

--- sgdlib/core/loss/base.hpp
+++ sgdlib/core/loss/base.hpp
@@ -15,19 +15,62 @@ public:
     LossFunction(LossParamType loss_param): loss_param_(loss_param) {};
     virtual ~LossFunction() {};
 
+    /* -------------without intercept term------------- */
+    /**
+     * @brief evaluate the loss function
+     * 
+     * @param[in] X 1darray of shape (num_samples * num_features), the matrix of input data
+     * @param[in] y 1darray of shape (num_samples) 
+     * @param[in] w 1darray of shape (num_features) coefficient of the features
+     * @param[in] b scalar intercept term
+     * @return loss function value
+    */
     virtual double evaluate(const std::vector<FeatureType>& X, 
                             const std::vector<LabelType>& y, 
-                            const std::vector<FeatureType>& weight, 
-                            const FeatureType& bias) const = 0 ;
-
+                            const std::vector<FeatureType>& w, 
+                            const FeatureType& b) const = 0 ;
+    
+    /**
+     * @brief compute gradient of the loss function
+     * 
+     * @param[in] X 1darray of shape (num_samples * num_features), the matrix of input data
+     * @param[in] y 1darray of shape (num_samples) 
+     * @param[in] w 1darray of shape (num_features) coefficient of the features
+     * @param[in] b scalar intercept term of the model
+     * @param[out] grad_w vector of FeatureType, where the gradient with respect to 
+     *      the weights will be stored.
+     * @param[out] grad_b FeatureType, where the gradient with respect to the bias
+    */
     virtual void gradient(const std::vector<FeatureType>& X, 
                           const std::vector<LabelType>& y, 
-                          const std::vector<FeatureType>& weight,
-                          const FeatureType& bias,
-                          std::vector<FeatureType>& weight_grad, 
-                          FeatureType& bias_grad) const = 0;
+                          const std::vector<FeatureType>& w,
+                          const FeatureType& b,
+                          std::vector<FeatureType>& grad_w, 
+                          FeatureType& grad_b) const = 0;
     
-    void set_param(const std::string& name, double value) {
+    /* -------------compute intercept term------------- */
+
+    /**
+     * evaluate the loss value of model
+     * 
+     * @param[in] X 1darray of shape (num_samples * num_features), the matrix of input data
+     * @param[in] y 1darray of shape (num_samples) 
+     * @param[in] w 1darray of shape (num_features) coefficient of the features
+    */
+    virtual double evaluate(const std::vector<FeatureType>& X, 
+                            const std::vector<LabelType>& y, 
+                            const std::vector<FeatureType>& w) const = 0 ;
+
+    /** 
+     * compute gradient of the loss function 
+    */
+    virtual void gradient(const std::vector<FeatureType>& X, 
+                          const std::vector<LabelType>& y, 
+                          const std::vector<FeatureType>& w,
+                          std::vector<FeatureType>& grad) const = 0;
+
+    // set loss function parameters
+    void set_param(const std::string& name, const double& value) {
         loss_param_[name] = value;
     }
 };
