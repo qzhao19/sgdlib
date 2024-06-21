a21c962b3ba195778c70d0ed23b0ce73ef2d0459
qzhao19 <qi.zhao@outlook.fr>
2024-06-21 23:48:48 +0800

    update

--- tests/test_loss.cpp
+++ tests/test_loss.cpp
@@ -24,7 +24,35 @@ public:
     std::unique_ptr<sgdlib::LossFunction> loss_fn; 
 };
 
-TEST_F(LossTest, LogLossTest) {
+TEST_F(LossTest, LogLossNoBias) {
+    std::vector<double> X = {5.2, 3.3, 1.2, 0.3,
+                            4.8, 3.1 , 1.6, 0.2,
+                            4.75, 3.1, 1.32, 0.1,
+                            5.9, 2.6, 4.1, 1.2,
+                            5.1, 2.2, 3.3, 1.1,
+                            5.2, 2.7, 4.1, 1.3,
+                            6.6, 3.1, 5.25, 2.2,
+                            6.3, 2.5, 5.1, 2.0,
+                            6.5, 3.1, 5.2, 2.1};
+    std::vector<long> y = {0, 0, 0, 0, 1, 1, 1, 1, 1};
+    std::vector<double> w = {0.9781, 0.9711, 0.3962, 0.5209};
+    double b = 1.0;
+
+    SetUp("LogLoss");
+    loss_fn->set_param("alpha", 1.0);
+    loss_fn->set_param("wscale", 1.0);
+    double loss = loss_fn->evaluate(X, y, w);
+    EXPECT_DOUBLE_EQ(loss, 4.7189764781884902);
+
+    std::vector<double> grad(4);
+    loss_fn->gradient(X, y, w, grad);
+    std::vector<double> expect = {4.2504766518182384, 3.2865449134325799, 
+                                  1.7056665980952996, 1.2417850159468702};
+    EXPECT_EQ(grad, expect);
+
+};
+
+TEST_F(LossTest, LogLossWithBias) {
     std::vector<double> X = {5.2, 3.3, 1.2, 0.3,
                             4.8, 3.1 , 1.6, 0.2,
                             4.75, 3.1, 1.32, 0.1,
