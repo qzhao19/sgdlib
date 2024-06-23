bf2e919c6cbbd45da4a5c2ee75ef8c7c836ac679
qzhao19 <qi.zhao@outlook.fr>
2024-06-23 23:01:35 +0800

    update

--- tests/test_loss.cpp
+++ tests/test_loss.cpp
@@ -24,62 +24,52 @@ public:
     std::unique_ptr<sgdlib::LossFunction> loss_fn; 
 };
 
-TEST_F(LossTest, LogLossNoBias) {
-    std::vector<double> X = {5.2, 3.3, 1.2, 0.3,
-                            4.8, 3.1 , 1.6, 0.2,
-                            4.75, 3.1, 1.32, 0.1,
-                            5.9, 2.6, 4.1, 1.2,
-                            5.1, 2.2, 3.3, 1.1,
-                            5.2, 2.7, 4.1, 1.3,
-                            6.6, 3.1, 5.25, 2.2,
-                            6.3, 2.5, 5.1, 2.0,
-                            6.5, 3.1, 5.2, 2.1};
-    std::vector<long> y = {0, 0, 0, 0, 1, 1, 1, 1, 1};
-    std::vector<double> w = {0.9781, 0.9711, 0.3962, 0.5209};
-    double b = 1.0;
+TEST_F(LossTest, LogLoss) {
+    std::vector<double> x1 = {5.2, 3.3, 1.2, 0.3};
+    std::vector<double> x2 = {6.4, 3.1, 5.5, 1.8};
+    long y1 = 1, y2 = 1;
+    std::vector<double> w = {1.0, 1.0, 1.0, 1.0};
+
+    double y1_pred, y2_pred;
+    y1_pred = std::inner_product(x1.begin(), 
+                                 x1.end(), 
+                                 w.begin(), 0.0);
+    y2_pred = std::inner_product(x2.begin(), 
+                                 x2.end(), 
+                                 w.begin(), 0.0);
 
     SetUp("LogLoss");
-    loss_fn->set_param("alpha", 1.0);
-    loss_fn->set_param("wscale", 1.0);
-    double loss = loss_fn->evaluate(X, y, w);
-    EXPECT_DOUBLE_EQ(loss, 4.7189764781884902);
+    double loss1 = loss_fn->evaluate(y1_pred, y1);
+    EXPECT_DOUBLES_EQUAL(loss1, 4.5398899216870535e-05);
 
-    std::vector<double> grad(4);
-    loss_fn->gradient(X, y, w, grad);
-    std::vector<double> expect = {4.2504766518182384, 3.2865449134325799, 
-                                  1.7056665980952996, 1.2417850159468702};
-    EXPECT_EQ(grad, expect);
+    double dloss1 = loss_fn->derivate(y1_pred, y1);
+    EXPECT_DOUBLES_EQUAL(dloss1, -4.5397868702434395e-05);
 
+    double loss2 = loss_fn->evaluate(y2_pred, y2);
+    EXPECT_DOUBLES_EQUAL(loss2, 5.0565312213476096e-08);
+
+    double dloss2 = loss_fn->derivate(y2_pred, y2);
+    EXPECT_DOUBLES_EQUAL(dloss2, -5.0565310926504413e-08);
+
+    double loss = (loss1 + loss2) / 2.0;
+    EXPECT_DOUBLES_EQUAL(loss, 2.272473226451872e-05);
+
+    std::vector<double> g(x1.size()), g1(x1.size()), g2(x1.size());
+    for (std::size_t i = 0; i < x1.size(); ++i) {
+        g1[i] = x1[i] * dloss1;
+        g2[i] = x2[i] * dloss2;
+        g[i] = (g1[i] + g2[i]) / 2.0;
+    }
+
+    double tol = 1e-6;
+    std::vector<double> expect_g = {-1.18196268e-04, -7.49848596e-05, 
+                                    -2.73777758e-05, -6.85518909e-06};
+    for (size_t i = 0; i < g.size(); ++i) {
+        EXPECT_NEAR(g[i], expect_g[i], tol);
+    }
 };
 
-TEST_F(LossTest, LogLossWithBias) {
-    std::vector<double> X = {5.2, 3.3, 1.2, 0.3,
-                            4.8, 3.1 , 1.6, 0.2,
-                            4.75, 3.1, 1.32, 0.1,
-                            5.9, 2.6, 4.1, 1.2,
-                            5.1, 2.2, 3.3, 1.1,
-                            5.2, 2.7, 4.1, 1.3,
-                            6.6, 3.1, 5.25, 2.2,
-                            6.3, 2.5, 5.1, 2.0,
-                            6.5, 3.1, 5.2, 2.1};
-    std::vector<long> y = {0, 0, 0, 0, 1, 1, 1, 1, 1};
-    std::vector<double> w = {0.9781, 0.9711, 0.3962, 0.5209};
-    double b = 1.0;
 
-    SetUp("LogLoss");
-    loss_fn->set_param("alpha", 1.0);
-    loss_fn->set_param("wscale", 1.0);
-    double loss = loss_fn->evaluate(X, y, w, b);
-    EXPECT_DOUBLE_EQ(loss, 4.7189764781884902);
 
-    double grad_b = 0.0;
-    std::vector<double> grad_w(4);
-    loss_fn->gradient(X, y, w, b, grad_w, grad_b);
-    std::vector<double> expect_w = {4.2504766518182384, 3.2865449134325799, 
-                                  1.7056665980952996, 1.2417850159468702};
-    EXPECT_EQ(grad_w, expect_w);
-
-    std::cout << grad_b << std::endl;
-};
 
 }
