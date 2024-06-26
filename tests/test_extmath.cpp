6701150e49ad8f9edb7a6f8b7e7390def2045393
qzhao19 <qi.zhao@outlook.fr>
2024-06-26 23:13:43 +0800

    update

--- tests/test_extmath.cpp
+++ tests/test_extmath.cpp
@@ -12,7 +12,7 @@ TEST(ExtmathTest, SigmoidTest){
     EXPECT_DOUBLE_EQ(x, 0.9999546021312976);
 };
 
-TEST(ExtmathTest, ClipTest){
+TEST(ExtmathTest, VectorClipTest){
     std::vector<double> x = {5.2, 3.3, 1.2, 0.3, 
                              4.8, 3.1, 1.6, 0.2};
     sgdlib::internal::clip<double>(x, 2.0, 4.0);
@@ -21,8 +21,13 @@ TEST(ExtmathTest, ClipTest){
     EXPECT_EQ(x, expect);
 };
 
-TEST(ExtmathTest, IsinfTest){
+TEST(ExtmathTest, ScalarClipTest){
+    double a = 5.2;
+    sgdlib::internal::clip<double>(a, 2.0, 4.0);
+    EXPECT_DOUBLE_EQ(a, 4.0);
+};
 
+TEST(ExtmathTest, VectorIsinfTest){
     double a = INF;
     std::vector<double> x1 = {5.2, 3.3, a, 0.3};
     bool has_inf = sgdlib::internal::isinf<double>(x1);
@@ -31,6 +36,12 @@ TEST(ExtmathTest, IsinfTest){
     std::vector<double> x2 = {5.2, 3.3, 2.6, 0.3};
     bool no_inf = sgdlib::internal::isinf<double>(x2);
     EXPECT_FALSE(no_inf);
+};
+
+TEST(ExtmathTest, ScalarIsinfTest){
+    double a = INF;
+    bool is_inf = sgdlib::internal::isinf<double>(a);
+    EXPECT_TRUE(is_inf);
 };
 
 TEST(ExtmathTest, Sqnorm2Test){
