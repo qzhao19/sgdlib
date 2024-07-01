447bd817d38cfd0d8359f925d7ac0347008ea8ed
qzhao19 <qi.zhao@outlook.fr>
2024-07-01 23:51:31 +0800

    update

--- sgdlib/algorithm/sgd/sag.hpp
+++ sgdlib/algorithm/sgd/sag.hpp
@@ -15,6 +15,7 @@ namespace sgdlib {
 class SAG: public BaseOptimizer {
 public:
     SAG(const std::vector<FeatureType>& x0, 
+        const FeatureType& b0,
         std::string loss, 
         std::string lr_policy,
         double alpha,
@@ -26,10 +27,11 @@ public:
         std::size_t num_iters_no_change,
         std::size_t random_seed,
         bool shuffle = true, 
-        bool verbose = true): BaseOptimizer(x0, 
+        bool verbose = true): BaseOptimizer(x0, b0,
             loss, lr_policy, 
             alpha, eta0, 
-            tol, gamma,
+            tol, 
+            gamma,
             max_iters, 
             batch_size, 
             num_iters_no_change,
@@ -44,10 +46,57 @@ public:
         std::size_t num_samples = y.size();
         std::size_t num_features = x0_.size();
 
+        // initialize x0 (weight) and b0 (bias)
         std::vector<FeatureType> x0 = x0_;
+        FeatureType b0 = b0_;
+
+        // 
         std::vector<FeatureType> sum_grad(num_features);
         std::vector<FeatureType> grad_history(num_features);
-        std::vector<FeatureType> last_updated(num_features);
+        std::vector<FeatureType> cumulative_sums(max_iters_ * num_samples);
+
+        std::vector<std::size_t> seen(num_samples);
+        std::vector<std::size_t> last_updated(num_features);
+        
+        std::size_t no_improvement_count = 0;
+        std::size_t sample_index = 0;
+        std::size_t iter = 0;
+        std::size_t num_seens = 0;
+
+        bool is_converged = false;
+        bool is_infinity = false;
+        double best_loss = INF;
+        FeatureType wscale = 1.0;
+
+        // initialize a lookup table for training X, y
+        std::vector<std::size_t> X_data_index(num_samples);
+        std::iota(X_data_index.begin(), X_data_index.end(), 0);
+
+        // initialize loss, loss_history, gradient, 
+        double loss, dloss;
+        FeatureType y_hat;
+        FeatureType bias_update = 0.0;
+        std::vector<double> loss_history(num_samples, 0.0);
+        std::vector<FeatureType> weight_update(num_features, 0.0);
+
+        for (iter = 0; iter < max_iters_; ++iter) {
+            for (std::size_t i = 0; i < num_samples; ++i) {
+                sample_index = random_state_.random<std::size_t>(X_data_index);
+
+                // update the number of X seen
+                if (seen[sample_index] == 0) {
+                    ++num_seens;
+                    seen[sample_index] = 1;
+                }
+                
+
+
+
+
+            }
+
+
+        }
 
     }
 
