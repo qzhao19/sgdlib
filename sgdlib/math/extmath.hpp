ab7fed5faebec66d21c47c5de69624a0befb8532
qzhao19 <qi.zhao@outlook.fr>
2024-07-03 22:57:57 +0800

    update

--- sgdlib/math/extmath.hpp
+++ sgdlib/math/extmath.hpp
@@ -9,6 +9,9 @@ namespace internal {
 /**
  * @brief compute sigmoid function 
  *      f(x) = 1/(1 + e^-x)
+ * 
+ * @tparam Type The type of elements for the input.
+ * 
  * @param x input value
 */
 template <typename Type>
@@ -19,8 +22,13 @@ inline Type sigmoid(Type x) {
 /**
  * @brief Clip (limit) the values in a vector.
  * 
+ * @tparam Type The type of elements in the vector.
+ * 
  * @param x vector containing elements to clip.
  * @param min, max minimum and maximum value
+ * 
+ * @note The function is marked as inline, which is suitable for small functions 
+ *       to reduce the overhead of function calls.
 */
 template<typename Type>
 inline void clip(std::vector<Type>& x, Type min, Type max) {
@@ -37,8 +45,13 @@ inline void clip(std::vector<Type>& x, Type min, Type max) {
 /** 
  * @brief Clips the value to the specified range [min, max]
  * 
+ * @tparam Type The type of elements for inputs.
+ * 
  * @param x The input value to be clipped.
  * @param min, max minimum and maximum value
+ * 
+ * @note The function is marked as inline, which is suitable for small functions 
+ *       to reduce the overhead of function calls.
 */
 template<typename Type>
 inline void clip(Type& x, Type min, Type max) {
@@ -46,12 +59,17 @@ inline void clip(Type& x, Type min, Type max) {
         throw std::invalid_argument("a_min must be less than or equal to a_max.");
     }
     x = std::max(min, std::min(x, max));
-}
+};
 
 /**
  * @brief check if any element of vector is infinite.
  * 
+ * @tparam Type The type of elements in the vector.
+ * 
  * @param x vector containing elements to check infinity.
+ * 
+ * @note The function is marked as inline, which is suitable for small functions 
+ *       to reduce the overhead of function calls.
 */
 template<typename Type>
 inline bool isinf(const std::vector<Type>& x) {
@@ -61,12 +79,17 @@ inline bool isinf(const std::vector<Type>& x) {
         }
     }
     return false;
-}
+};
 
 /**
  * @brief check if the given value represents infinity
  * 
+ * @tparam Type The type of elements for input.
+ * 
  * @param x value to check infinity.
+ * 
+ * @note The function is marked as inline, which is suitable for small functions 
+ *       to reduce the overhead of function calls.
  */
 template<typename Type>
 inline bool isinf(const Type& x) {
@@ -74,20 +97,25 @@ inline bool isinf(const Type& x) {
         return true;
     }
     return false;
-}
+};
 
 /**
  * @brief calculate the L2 norm of a vector.
  * 
+ * @tparam Type The type of elements in the vector.
+ * 
  * @param x a vector of type T.
  * @return The L2 norm of the vector as a double.
+ * 
+ * @note The function is marked as inline, which is suitable for small functions 
+ *       to reduce the overhead of function calls.
  */
 template<typename Type>
 inline double sqnorm2(const std::vector<Type>& x) {
     return std::sqrt(
         std::inner_product(x.begin(), x.end(), x.begin(), 0.0)
     );
-}
+};
 
 /**
  * @brief Computes the index of the maximum element in a given array.
@@ -96,7 +124,11 @@ inline double sqnorm2(const std::vector<Type>& x) {
  *         The array should contain 'size' number of elements of type ValueType.
  * @param size The number of elements in the array pointed to by 'x'.
  * @return IndexType The index of the maximum element within the array.
- *         If multiple elements are equal to the maximum value, the index of the first occurrence is returned.
+ *         If multiple elements are equal to the maximum value, the index 
+ *         of the first occurrence is returned.
+ * 
+ * @note The function is marked as inline, which is suitable for small functions 
+ *       to reduce the overhead of function calls.
  */
 template<typename ValueType, typename IndexType>
 inline IndexType argmax(ValueType* x, unsigned long size) {
@@ -115,15 +147,52 @@ inline IndexType argmax(ValueType* x, unsigned long size) {
 /** 
  * @brief Applies a scalar multiplication operation to a vector.
  * 
+ * @tparam Type The type of elements in the vector.
+ * 
  * @param[in,out] x vector of type T, which will be scaled by the scalar 'c'.
  * @param[in] scalar constant scalar value 
+ * 
+ * @note The function is marked as inline, which is suitable for small functions 
+ *       to reduce the overhead of function calls.
 */
-template<typename T>
-inline void dot(std::vector<T>& x, const T c) {
+template<typename Type>
+inline void dot(std::vector<Type>& x, const Type c) {
     for (std::size_t i = 0; i < x.size(); ++i) {
         x[i] *= c;
     }
-}
+};
+
+/** 
+ * @brief Applies a scalar multiplication operation to a vector. 
+ * It computes the sum of the products of all elements within 
+ * the iterator range from begin to end with a constant c.
+ * 
+ * @tparam Type The type of elements in the vector.
+ * @tparam IterType The type of iterator, with a default of std::vector<Type>::const_iterator.
+ * 
+ * @param begin The beginning iterator pointing to the first element 
+ *      of the vector to calculate the dot product.
+ * @param end The ending iterator pointing to the first element following begin.
+ * @param c The constant to be multiplied with the elements of the vector.
+ * @param out The reference to a vector that stores the result of the dot product.
+ * 
+ * @note The function is marked as inline, which is suitable for small functions 
+ *       to reduce the overhead of function calls.
+ * @note The use of templates and default iterator types allows the function 
+ *       to be used with different types of vectors and iterators.
+ * 
+*/
+template<typename Type, 
+         typename IterType = typename std::vector<Type>::const_iterator>
+inline void dot(IterType begin, IterType end, 
+                const Type c, 
+                std::vector<Type>& out) {
+    std::transform(begin, end, 
+                   out.begin(), 
+                   [c](const Type& elem) {
+                        return elem * c;
+                   });
+};
 
 } // namespace internal
 } // namespace sgdlib
