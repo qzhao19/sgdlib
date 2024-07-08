fe216e5923ad9e47b269053d98b9e22633e4865e
qzhao19 <qi.zhao@outlook.fr>
2024-07-08 22:53:24 +0800

    add base.hpp for step size search

---
+++ sgdlib/core/stepsize_search/base.hpp
@@ -0,0 +1,32 @@
+#ifndef CORE_STEPSIZE_SEARCH_BASE_HPP_
+#define CORE_STEPSIZE_SEARCH_BASE_HPP_
+
+#include "common/prereqs.hpp"
+#include "common/predefs.hpp"
+#include "common/registry.hpp"
+#include "core/loss.hpp"
+#include "math/extmath.hpp"
+
+namespace sgdlib {
+
+class StepSizeSearch {
+protected:
+    std::vector<FeatureType> X_;
+    std::vector<LabelType> y_;
+    StepSizeSearchParamType stepsize_search_param_;
+    
+public:
+    StepSizeSearch(const std::vector<FeatureType>& X, 
+                   const std::vector<LabelType>& y,
+                   StepSizeSearchParamType stepsize_search_param): X_(X), y_(y),
+                stepsize_search_param_(stepsize_search_param) {};
+    ~StepSizeSearch() {};
+
+    virtual int search(bool is_saga, double& step_size) {
+        return 0;
+    };
+};
+
+} // namespace sgdlib
+
+#endif // CORE_STEPSIZE_SEARCH_BASE_HPP_
