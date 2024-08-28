67e2b8a97833ea6e03610c54a3f76c673d8ed2b8
qzhao19 <qi.zhao@outlook.fr>
2024-08-28 21:34:23 +0800

    update

--- sgdlib/algorithm/sgd/scd.hpp
+++ sgdlib/algorithm/sgd/scd.hpp
@@ -8,7 +8,7 @@ namespace sgdlib {
 /**
  * @file scd.hpp
  * 
- * @brief Stochastic Coordinate Descent (SAGD) optimizer.
+ * @brief Stochastic Coordinate Descent (SCD) optimizer.
  * 
 */
 class SCD: public BaseOptimizer {
@@ -35,7 +35,7 @@ public:
                   const std::vector<LabelType>& y) override {
         
         std::size_t num_samples = y.size();
-        std::size_t num_features = w0.size();
+        std::size_t num_features = w0_.size();
 
         // initialize w0 (weight) and b0 (bias)
         std::vector<FeatureType> w0 = w0_;
@@ -50,24 +50,30 @@ public:
         bool is_converged = false;
         FeatureType prev_weight;
 
-        // initialize a lookup table for training X, y
-        std::vector<std::size_t> X_feature_index(num_samples);
-        std::iota(X_feature_index.begin(), X_feature_index.end(), 0);
-
         std::size_t iter = 0;
         std::size_t feature_index = 0;
 
         // compute column-wise norm2
         std::vector<FeatureType> X_col_norm(num_features);
-        sgdlib::internal::col_norms<FeatureType>(X, true, X_col_norm);
+        sgdlib::internal::col_norms<FeatureType>(X, false, X_col_norm);
 
-        FeatureType wmax = 0.0;
+
+        FeatureType max_weight, max_weight_update;
         for (iter = 0; iter < max_iters_; ++iter) {
+            max_weight = 0.0;
+            max_weight_update = 0.0;
+
+            std::size_t best_feature_index;
+            FeatureType best_weight_update;
+            FeatureType best_descent = -1.0;
+            FeatureType pred_descent;
+
             // cycle through all the features
             for (std::size_t j = 0; j < num_features; ++j) {
 
                 // choose a feature index randomly
-                feature_index = random_state_.random_index(0, num_features);
+                // feature_index = random_state_.random_index(0, num_features);
+                feature_index = j;
 
                 // if norms of the columns of X is null
                 if (X_col_norm[feature_index] == 0.0) {
@@ -79,8 +85,12 @@ public:
 
                 dloss = 0.0;
                 for (std::size_t i = 0; i < num_samples; ++i) {
+                    // std::cout << "xi_w[i] = " << xi_w[i] << std::endl;
+                    // std::cout << "loss_fn_->derivate(xi_w[i], y[i]) = " << loss_fn_->derivate(0.0, 1) << std::endl;
                     dloss += loss_fn_->derivate(xi_w[i], y[i]) * X[i * num_features + feature_index];
                 }
+
+                // std::cout << "dloss = " << dloss << std::endl;
 
                 // compute gradient for target feature X[:, feature_index]
                 grad = dloss / static_cast<FeatureType>(num_samples);
@@ -96,30 +106,44 @@ public:
                     weight_update = -w0[feature_index];
                 }
 
+                pred_descent = -weight_update*grad - rho_ / 2.0 * weight_update * weight_update - \
+                    alpha_ * (std::abs(w0[feature_index] + weight_update) - std::abs(w0[feature_index]));
+
+                if (pred_descent > best_descent) {
+                    best_feature_index = feature_index;
+                    best_weight_update = weight_update;
+                    best_descent = pred_descent;
+                }
+
+                // max abs-coeff update
+                max_weight = std::fmax(max_weight, w0[feature_index]); 
+                max_weight_update = std::fmax(max_weight_update, std::abs(w0[feature_index] - prev_weight)); 
             }
+            feature_index = best_feature_index;
+            weight_update = best_weight_update;
 
             // update weight vector w
             w0[feature_index] += weight_update;
 
             // update inner product xi_w
             for (std::size_t i = 0; i < num_samples; ++i) {
-                // dloss += loss_fn_->derivate(xi_w[i], y[i]) * X[i * feature_index];
                 xi_w[i] = xi_w[i] + weight_update * X[i * num_features + feature_index];
             }
 
-            // print the summary
+            // print info
             if (verbose_) {
                 for (std::size_t i = 0; i < num_samples; ++i) {
-                    loss += loss_fn_->evaluate(xi_w[i], y[i])
+                    loss += loss_fn_->evaluate(xi_w[i], y[i]);
                 }
                 std::cout << "Epoch = " << (iter + 1) << ", wnorm1 = " 
                           << sgdlib::internal::norm1<FeatureType>(w0) << ", avg loss = " 
                           << loss / static_cast<FeatureType>(num_samples) << std::endl;
             }
 
-            // convergence check
-            wmax = std::fmax(wmax, std::abs(w0[feature_index] - prev_weight));
-            if (wmax < tol_) {
+            // convergence check maximum coordinate update
+            // max_j|wj_new - wj_old| < tol * max_abs_coef_update
+            // https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.Lasso.html
+            if ((max_weight_update / max_weight > tol_) || (iter == max_iters_ - 1)) {
                 is_converged = false;
                 break;
             }
@@ -132,8 +156,9 @@ public:
             throw std::runtime_error(err_msg.str());
         }
 
-    }
+        w_opt_ = w0;
 
+    }
 };
 
 }
