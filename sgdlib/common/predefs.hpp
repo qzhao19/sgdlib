b7033f38a18d5d46bd8bf905e7d5c19d9a0e75a8
qzhao19 <qi.zhao@outlook.fr>
2024-12-11 23:40:20 +0800

    update

--- sgdlib/common/predefs.hpp
+++ sgdlib/common/predefs.hpp
@@ -38,6 +38,16 @@ constexpr double inf = std::numeric_limits<double>::infinity();
 #define UNUSED __attribute__((__unused__))
 #define USED __attribute__((__used__))
 
+/**
+ * @brief Demangles a C++ mangled name to its human-readable form.
+ *
+ * This function uses the C++ API's demangling functionality to convert a mangled C++ name
+ * into its human-readable form. If the demangling is successful, the demangled name is
+ * returned as a std::string. If the demangling fails, the original mangled name is returned.
+ *
+ * @param name The mangled C++ name to be demangled.
+ * @return The demangled name as a std::string, or the original mangled name if demangling fails.
+ */
 std::string Demangle(const char* name) {
     int status;
     auto demangled = ::abi::__cxa_demangle(name, nullptr, nullptr, &status);
@@ -50,6 +60,13 @@ std::string Demangle(const char* name) {
     return name;
 }
 
+/**
+ * @brief Demangles the type name of a given template parameter.
+ *
+ * @tparam Type The template parameter whose type name needs to be demangled.
+ * @return A C-style string containing the demangled type name or a static message indicating
+ *         the issue with RTTI.
+ */
 template <typename Type>
 static const char* DemangleType() {
 #ifdef __GXX_RTTI
