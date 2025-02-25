2ea3c5140f2927af0906efaea24021f382f9f29c
qzhao19 <qi.zhao@outlook.fr>
2025-02-25 22:45:36 +0800

    create backtracking_line_search

---
+++ sgdlib/core/stepsize_search/backtracking_line_search.hpp
@@ -0,0 +1,16 @@
+#ifndef CORE_STEPSIZE_SEARCH_BACKTRACKING_LINE_SEARCH_HPP_
+#define CORE_STEPSIZE_SEARCH_BACKTRACKING_LINE_SEARCH_HPP_
+
+#include "base.hpp"
+
+namespace sgdlib {
+
+template <typename LossFuncType>
+class BacktrackingLineSearch final: public StepSizeSearch<LossFuncType> {
+
+
+};
+
+} // namespace sgdlib
+
+#endif // CORE_STEPSIZE_SEARCH_BACKTRACKING_LINE_SEARCH_HPP_
