a1bc15efce21060166bd277936b89d697bb46b3f
qzhao19 <qi.zhao@outlook.fr>
2025-03-01 21:55:06 +0800

    update

--- sgdlib/core/stepsize_search/base.hpp
+++ sgdlib/core/stepsize_search/base.hpp
@@ -26,13 +26,13 @@ protected:
 
     std::vector<FeatureType> X_;
     std::vector<LabelType> y_;
-    StepSizeSearchParamType stepsize_search_params_;
+    StepSizeSearchParamType *stepsize_search_params_;
     
 public:  
     StepSizeSearch(const std::vector<FeatureType>& X, 
                    const std::vector<LabelType>& y,
                    const std::shared_ptr<LossFuncType>& loss_fn,
-                   StepSizeSearchParamType stepsize_search_params): X_(X), y_(y),
+                   StepSizeSearchParamType *stepsize_search_params): X_(X), y_(y),
                         loss_fn_(loss_fn),
                         stepsize_search_params_(stepsize_search_params){};
     
@@ -50,6 +50,17 @@ public:
                        double& stepsize) {
         return 0;
     };
+
+    virtual int search(const std::vector<FeatureType>& xp, 
+                       const std::vector<FeatureType>& gp, 
+                       std::vector<FeatureType>& x,
+                       std::vector<FeatureType>& g, 
+                       std::vector<FeatureType>& d,
+                       FeatureType& fx,
+                       double& stepsize) {
+        return 0;
+    };
+
 };
 
 } // namespace sgdlib
