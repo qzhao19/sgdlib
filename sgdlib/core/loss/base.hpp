60fbba65dd4889380f501cae61f945e9c290f14b
qzhao19 <qi.zhao@outlook.fr>
2025-03-08 22:03:17 +0800

    update

--- sgdlib/core/loss/base.hpp
+++ sgdlib/core/loss/base.hpp
@@ -30,7 +30,7 @@ public:
      *      The predicted output of a model given an input.
      * @param y_true LabelValType
      *      The true label value for the data point
-     * @return A FloatValType value for loss function value
+     * @return A FloatType value for loss function value
     */
     virtual FeatValType evaluate(const FeatValType& y_pred, 
                                  const LabelValType& y_true) const = 0;
@@ -42,7 +42,7 @@ public:
      *      The predicted output of a model given an input.
      * @param y_true LabelValType
      *      The true label value for the data point
-     * @return A FloatValType value representing the derivative of the loss function
+     * @return A FloatType value representing the derivative of the loss function
     */
     virtual FeatValType derivate(const FeatValType& y_pred, 
                                  const LabelValType& y_true) const = 0;
@@ -51,9 +51,9 @@ public:
      * @brief Sets a parameter for the loss function.
      * 
      * @param name The name of the parameter as a string.
-     * @param value The value of the parameter as a FloatValType.
+     * @param value The value of the parameter as a FloatType.
     */
-    void set_param(const std::string& name, const FloatValType& value) {
+    void set_param(const std::string& name, const FloatType& value) {
         loss_param_[name] = value;
     }
 };
