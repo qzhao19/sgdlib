8762bfee26ceb700a36fd70dde92e1f701c72912
qzhao19 <qi.zhao@outlook.fr>
2025-03-16 22:35:31 +0800

    add bracketing_line_search

--- sgdlib/core/stepsize_search/bracketing_line_search.hpp
+++ sgdlib/core/stepsize_search/bracketing_line_search.hpp
@@ -25,16 +25,113 @@ public:
     ~BracketingLineSearch() = default;
 
     int search(const std::vector<FeatValType>& xp, 
-        const std::vector<FeatValType>& gp, 
-        std::vector<FeatValType>& x,
-        std::vector<FeatValType>& g, 
-        std::vector<FeatValType>& d,
-        FeatValType& fx,
-        FloatType& stepsize) {
+               const std::vector<FeatValType>& gp, 
+               const std::vector<FeatValType>& d,
+               std::vector<FeatValType>& x,
+               std::vector<FeatValType>& g, 
+               FeatValType& fx,
+               FloatType& stepsize) override {
         
-        return 0; 
-    }
+        num_features_ = x.size();
+        
+        FloatType dec_factor = this->stepsize_search_params_->dec_factor;
+        FloatType inc_factor = this->stepsize_search_params_->inc_factor;
 
+        if (stepsize <= 0.0) {
+            // step must be positive
+            return LBFGS_ERROR_INVALID_PARAMETERS;
+        }
+
+        // initialize fx_init and compute init gradient in search direction
+        FeatValType fx_init = fx;
+        FeatValType dg_init; 
+        sgdlib::internal::dot<FeatValType>(d, g, dg_init);
+
+        if (dg_init > 0.0) {
+            // moving direction increases the objective function value
+            return LBFGS_ERROR_INCREASE_GRADIENT;
+        }
+
+        FloatType dg_test = this->stepsize_search_params_->ftol * dg_init;
+        FloatType stepsize_hi = INF; 
+        FloatType stepsize_lo = 0.0;
+        // define loss and grad vector
+        FeatValType y_hat;
+        FeatValType loss = 0.0;
+        std::vector<FeatValType> grad(num_features_, 0.0);
+        
+        int count = 0;
+        while (true) {
+            // x_{k+1} = x_k + stepsize * d_k
+            std::transform(xp.begin(), xp.end(), d.begin(), x.begin(),
+               [stepsize](auto xval, auto dval) {
+                   return xval + stepsize * dval;
+               });
+            
+            loss = 0.0;
+            std::memset(grad.data(), 0, grad.size() * sizeof(FeatValType));
+
+            // compute the loss value and gradient vector
+            for (std::size_t i = 0; i < num_samples_; ++i) {
+                y_hat = std::inner_product(&this->X_[i * num_features_], 
+                                           &this->X_[(i + 1) * num_features_], 
+                                           x.begin(), 0.0);
+                loss += this->loss_fn_->evaluate(y_hat, this->y_[i]);
+                for (std::size_t j = 0; j < num_features_; ++j) {
+                    grad[j] += this->loss_fn_->derivate(y_hat, this->y_[i]) * this->X_[i * num_features_ + j];
+                }
+            }
+            loss /= static_cast<FeatValType>(num_samples_);
+            std::transform(grad.begin(), grad.end(), grad.begin(),
+                          [this](FeatValType val) { 
+                            return val / static_cast<FeatValType>(num_samples_); 
+                        });
+            fx = loss;
+            g = grad;
+
+            ++count;
+
+            if (fx > fx_init + stepsize * dg_test) {
+                stepsize_hi = stepsize;
+            }
+            else {
+                // check the armijo condition
+                if (this->stepsize_search_params_->condition == "ARMIJO") {
+                    return count;
+                }
+                FloatType dg;
+                sgdlib::internal::dot<FloatType>(d, g, dg);
+                if (dg < this->stepsize_search_params_->wolfe * dg_init) {
+                    stepsize_lo = stepsize;
+                }
+                else {
+                    if (this->stepsize_search_params_->condition == "WOLFE") {
+                        return count;
+                    }
+
+                    if (dg > (-this->stepsize_search_params_->wolfe * dg_init)) {
+                        stepsize_hi = stepsize;
+                    }
+                    else {
+                        return count;
+                    }
+                }
+            }
+
+            if (stepsize < this->stepsize_search_params_->min_stepsize) {
+                return LBFGS_ERROR_MINIMUM_STEPSIZE;
+            }
+
+            if (stepsize > this->stepsize_search_params_->max_stepsize) {
+                return LBFGS_ERROR_MAXIMUM_STEPSIZE;
+            }
+
+            if (count >= this->stepsize_search_params_->max_searches) {
+                return LBFGS_ERROR_MAXIMUM_SEARCHES;
+            }
+            stepsize = std::isinf(stepsize_hi) ? 2.0 * stepsize : (stepsize_lo + stepsize_hi) / 2.0;;
+        }
+    }
 };
 
 } // namespace sgdlib
