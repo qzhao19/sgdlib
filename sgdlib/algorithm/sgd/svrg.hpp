763b2cd9aadc9d9730feaf901cadc210fc607e51
qzhao19 <qi.zhao@outlook.fr>
2025-03-22 23:56:59 +0800

    update

--- sgdlib/algorithm/sgd/svrg.hpp
+++ sgdlib/algorithm/sgd/svrg.hpp
@@ -22,7 +22,6 @@ public:
      * base class `BaseOptimizer`.
      *
      * @param w0 Initial weight vector for the model.
-     * @param b0 Initial bias term for the model.
      * @param loss The loss function to be minimized.
      * @param lr_policy The learning rate policy (e.g., constant, adaptive).
      * @param alpha L2 regularization parameter.
@@ -30,8 +29,7 @@ public:
      * @param tol Tolerance for convergence.
      * @param gamma Decay factor for the learning rate (used in some learning rate policies).
      * @param max_iters Maximum number of iterations for optimization.
-     * @param batch_size Size of the mini-batch used for gradient computation.
-     * @param num_iters_no_change Number of iterations with no improvement to wait before stopping.
+     * @param num_inner Size of the mini-batch used for gradient computation.
      * @param random_seed Seed for the random number generator.
      * @param shuffle If true, shuffles the data before each epoch (default: true).
      * @param verbose If true, enables logging of optimization progress (default: true).
@@ -41,7 +39,6 @@ public:
      * @see BaseOptimizer
      */
     SVRG(const std::vector<FeatValType>& w0, 
-        const FeatValType& b0,
         std::string loss, 
         std::string lr_policy,
         FloatType alpha,
@@ -49,18 +46,16 @@ public:
         FloatType tol,
         FloatType gamma,
         std::size_t max_iters, 
-        std::size_t batch_size,
-        std::size_t num_iters_no_change,
+        std::size_t num_inner,
         std::size_t random_seed,
         bool shuffle = true, 
-        bool verbose = true): BaseOptimizer(w0, b0,
+        bool verbose = true): BaseOptimizer(w0,
             loss, lr_policy, 
             alpha, eta0, 
             tol, 
             gamma,
             max_iters, 
-            batch_size, 
-            num_iters_no_change,
+            num_inner, 
             random_seed,
             shuffle, 
             verbose) {};
@@ -78,7 +73,8 @@ public:
         std::size_t num_samples = y.size();
         std::size_t num_features = this->w0_.size();
         std::size_t iter = 0;
-        
+        std::size_t batch_size = num_samples / this->num_inner_;
+
         // initialize a lookup table for training X, y
         std::vector<std::size_t> X_data_index(num_samples);
         std::iota(X_data_index.begin(), X_data_index.end(), 0);
@@ -86,7 +82,6 @@ public:
         // initialize loss, loss_history, gradient, 
         FeatValType y_hat;
         FeatValType loss, dloss;
-        FeatValType bias_update = 0.0;
         std::vector<FeatValType> loss_history(num_samples, 0.0);
         std::vector<FeatValType> weight_update(num_features, 0.0);
 
@@ -94,6 +89,8 @@ public:
         bool is_infinity = false;
         FloatType best_loss = INF;
         FeatValType wscale = 1.0;
+        // lr for regularization coefficient
+        FloatType eta_alpha = this->eta0_ * this->alpha_;
 
         // initialize a lookup table for training X, y
         std::vector<std::size_t> X_data_index(num_samples);
@@ -102,13 +99,13 @@ public:
         // initialize loss, loss_history, gradient, 
         FeatValType y_hat;
         FeatValType loss, dloss;
-        FeatValType bias_update = 0.0;
         std::vector<FeatValType> loss_history(this->max_iters_, 0.0);
         std::vector<FeatValType> weight_update(num_features, 0.0);
+        std::vector<FeatValType> full_weight_update(num_features, 0.0);
 
         // initialize w0 (weight) and b0 (bias)
         std::vector<FeatValType> w0 = this->w0_;
-        FeatValType b0 = this->b0_;
+        std::vector<FeatValType> init_w0 = w0;
 
 
         for (iter = 0; iter < this->max_iters_; ++iter) {
@@ -116,25 +113,43 @@ public:
             if (this->shuffle_) {
                 this->random_state_.shuffle<std::size_t>(X_data_index);
             }
-
-            // compute intial loss value and gradeint for full data
+            // compute intial gradeint for full data
+            // update full_weight_update is zero for beginning of iteration
+            std::memset(full_weight_update.data(), 0, full_weight_update.size() * sizeof(FeatValType));
             for (std::size_t i = 0; i < num_samples; ++i) {
-                y_hat = std::inner_product(&X[X_data_index[i] * num_features], 
-                                           &X[(X_data_index[i] + 1) * num_features], 
+                y_hat = std::inner_product(&X[i * num_features], 
+                                           &X[(i + 1) * num_features], 
                                            w0.begin(), 0.0);
-                y_hat = y_hat * wscale + b0;
+                y_hat = y_hat * wscale;
 
-                loss += this->loss_fn_->evaluate(y_hat, y[i]);
                 for (std::size_t j = 0; j < num_features; ++j) {
-                    weight_update[j] += this->loss_fn_->derivate(y_hat, y[X_data_index[i]]) * X[i * num_features + j];
+                    full_weight_update[j] += this->loss_fn_->derivate(y_hat, y[i]) * X[i * num_features + j];
                 }
             }
-            loss /= static_cast<FeatValType>(num_samples);
-            std::transform(weight_update.begin(), weight_update.end(), weight_update.begin(),
+            std::transform(full_weight_update.begin(), full_weight_update.end(), full_weight_update.begin(),
                           [num_samples](FeatValType val) { 
                                 return val / static_cast<FeatValType>(num_samples); 
                           });
+            // assign w0 to init_w0 vector
+            for (std::size_t j = 0; j < num_features; ++j) {
+                init_w0[j] = w0[j];
+            }
+            
+            // apply lr decay policy to compute eta
+            const FloatType eta = this->lr_decay_->compute(iter); 
+            // start inner loop
+            for (std::size_t n = 0; n < this->num_inner_; ++n) {
+                for (std::size_t m = 0; m < batch_size; ++m) {
+                    // compute predicted label proba XW
+                    y_hat = std::inner_product(&X[X_data_index[n * this->batch_size_ + m] * num_features], 
+                                               &X[(X_data_index[n * this->batch_size_ + m] + 1) * num_features], 
+                                               w0.begin(), 0.0);
+                    y_hat = y_hat * wscale;
 
+                    
+                }
+
+            }
 
 
         }
