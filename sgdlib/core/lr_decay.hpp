a709e3446c39f1594a4a351ae5dffcc12dcb8ad0
qzhao19 <qi.zhao@outlook.fr>
2025-04-01 23:41:37 +0800

    add namespace 'detail'

--- sgdlib/core/lr_decay.hpp
+++ sgdlib/core/lr_decay.hpp
@@ -5,10 +5,12 @@
 #include "lr_decay/invscaling_decay.hpp"
 
 namespace sgdlib {
+namespace detail {
 
 REGISTER_CLASS(LRDecayRegistry, Invscaling, Invscaling);
 REGISTER_CLASS(LRDecayRegistry, Exponential, Exponential);
 
-}
+} // namespace detail
+} // namespace sgdlib
 
 #endif // CORE_LR_DECAY_HPP_
