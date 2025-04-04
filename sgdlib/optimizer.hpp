2b96f79ac95ffd9e414322971c09aa27e0f58a76
qzhao19 <qi.zhao@outlook.fr>
2025-04-04 22:59:14 +0800

    open dataset_manager to public

--- sgdlib/optimizer.hpp
+++ sgdlib/optimizer.hpp
@@ -1,4 +1,4 @@
-#include "sgdlib/core/data/dataset_manager.hpp"
+#include "sgdlib/data/dataset_manager.hpp"
 #include "sgdlib/algorithm/sgd/sgd.hpp"
 #include "sgdlib/algorithm/sgd/sag.hpp"
 #include "sgdlib/algorithm/sgd/scd.hpp"
