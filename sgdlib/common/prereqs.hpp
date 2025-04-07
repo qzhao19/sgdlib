f9bf743361bd0632d52d23660d382854e4a9dde9
qzhao19 <qi.zhao@outlook.fr>
2025-04-07 23:50:12 +0800

    add USE_SIMD macro flag

--- sgdlib/common/prereqs.hpp
+++ sgdlib/common/prereqs.hpp
@@ -25,4 +25,42 @@
 
 #include <cxxabi.h>
 
+#if defined(USE_SIMD)
+    #if !defined(__GNUC__) || defined(__clang__)
+        #error "SIMD optimization requires GCC compiler (clang not supported)"
+    #endif
+
+    // check SSE4.1
+    #if !defined(__SSE4_1__)
+        #error "SSE4.1 is required when USE_SIMD is defined. Compile with -msse4.1"
+    #else
+        #include <smmintrin.h>
+        #define HAS_SSE4_1 1
+    #endif
+
+    // check AVX2（optional）
+    #if defined(__AVX2__)
+        #include <immintrin.h>
+        #define HAS_AVX2 1
+    #else
+        #define HAS_AVX2 0
+        #pragma message("AVX2 not available (will use SSE4.1)")
+    #endif
+
+    // define the active SIMD level
+    // Case 1: use SIMD（AVX2 level 2，SSE4.1 level 1)
+    #if HAS_AVX2
+        #define SIMD_TARGET_LEVEL 2  // AVX2
+    #else
+        #define SIMD_TARGET_LEVEL 1  // SSE4.1
+    #endif
+
+// Case 2: use scalar
+#else
+    #define SIMD_TARGET_LEVEL 0
+    #define HAS_SSE4_1 0
+    #define HAS_AVX2 0
+#endif // USE_SIMD
+
+
 #endif // COMMON_PREREQS_HPP_
