3df4ca4d1626895348707704543ca676b9305be5
qzhao19 <qi.zhao@outlook.fr>
2025-04-15 23:27:27 +0800

    update

--- sgdlib/core/stepsize_search/bracketing_line_search.hpp
+++ sgdlib/core/stepsize_search/bracketing_line_search.hpp
@@ -13,28 +13,28 @@ protected:
     std::size_t num_features_;
 
 public:
-    BracketingLineSearch(const std::vector<FeatValType>& X, 
+    BracketingLineSearch(const std::vector<FeatValType>& X,
                          const std::vector<LabelValType>& y,
                          const std::shared_ptr<LossFuncType>& loss_fn,
                             StepSizeSearchParamType* stepsize_search_params): StepSizeSearch<LossFuncType>(
-                                X, y, 
-                                loss_fn, 
+                                X, y,
+                                loss_fn,
                                 stepsize_search_params) {
         num_samples_ = y.size();
-        
+
     };
     ~BracketingLineSearch() = default;
 
-    int search(const std::vector<FeatValType>& xp, 
-               const std::vector<FeatValType>& gp, 
+    int search(const std::vector<FeatValType>& xp,
+               const std::vector<FeatValType>& gp,
                const std::vector<FeatValType>& d,
                std::vector<FeatValType>& x,
-               std::vector<FeatValType>& g, 
+               std::vector<FeatValType>& g,
                FeatValType& fx,
                FloatType& stepsize) override {
-        
+
         num_features_ = x.size();
-        
+
         FloatType dec_factor = this->stepsize_search_params_->dec_factor;
         FloatType inc_factor = this->stepsize_search_params_->inc_factor;
 
@@ -45,7 +45,7 @@ public:
 
         // initialize fx_init and compute init gradient in search direction
         FeatValType fx_init = fx;
-        FeatValType dg_init; 
+        FeatValType dg_init;
         sgdlib::detail::dot<FeatValType>(d, g, dg_init);
 
         if (dg_init > 0.0) {
@@ -54,13 +54,13 @@ public:
         }
 
         FloatType dg_test = this->stepsize_search_params_->ftol * dg_init;
-        FloatType stepsize_hi = INF; 
+        FloatType stepsize_hi = INF;
         FloatType stepsize_lo = 0.0;
         // define loss and grad vector
         FeatValType y_hat;
         FeatValType loss = 0.0;
         std::vector<FeatValType> grad(num_features_, 0.0);
-        
+
         int count = 0;
         while (true) {
             // x_{k+1} = x_k + stepsize * d_k
@@ -68,14 +68,14 @@ public:
                [stepsize](auto xval, auto dval) {
                    return xval + stepsize * dval;
                });
-            
+
             loss = 0.0;
             std::memset(grad.data(), 0, grad.size() * sizeof(FeatValType));
 
             // compute the loss value and gradient vector
             for (std::size_t i = 0; i < num_samples_; ++i) {
-                y_hat = std::inner_product(&this->X_[i * num_features_], 
-                                           &this->X_[(i + 1) * num_features_], 
+                y_hat = std::inner_product(&this->X_[i * num_features_],
+                                           &this->X_[(i + 1) * num_features_],
                                            x.begin(), 0.0);
                 loss += this->loss_fn_->evaluate(y_hat, this->y_[i]);
                 for (std::size_t j = 0; j < num_features_; ++j) {
@@ -84,8 +84,8 @@ public:
             }
             loss /= static_cast<FeatValType>(num_samples_);
             std::transform(grad.begin(), grad.end(), grad.begin(),
-                          [this](FeatValType val) { 
-                            return val / static_cast<FeatValType>(num_samples_); 
+                          [this](FeatValType val) {
+                            return val / static_cast<FeatValType>(num_samples_);
                         });
             fx = loss;
             g = grad;
