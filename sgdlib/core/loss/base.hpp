a1fe27a63d02cdd766aa09e9f6621ac8260daba7
qzhao19 <qi.zhao@outlook.fr>
2025-05-19 22:31:22 +0800

    update code

--- sgdlib/core/loss/base.hpp
+++ sgdlib/core/loss/base.hpp
@@ -1,7 +1,7 @@
 #ifndef CORE_LOSS_BASE_HPP_
 #define CORE_LOSS_BASE_HPP_
 
-#include "common/consts.hpp"
+#include "common/constants.hpp"
 #include "common/prereqs.hpp"
 #include "common/predefs.hpp"
 #include "common/registry.hpp"
@@ -10,13 +10,13 @@
 namespace sgdlib {
 namespace detail {
 
-/** 
+/**
  * @file base.hpp
- * 
+ *
  * @class LossFunction
- * 
+ *
  * @brief Abstract base class representing a loss function
- * 
+ *
 */
 class LossFunction {
 protected:
@@ -27,31 +27,31 @@ public:
 
     /**
      * evaluate the loss value of loss function
-     * 
+     *
      * @param y_pred FeatValType
      *      The predicted output of a model given an input.
      * @param y_true LabelValType
      *      The true label value for the data point
      * @return A FloatType value for loss function value
     */
-    virtual FeatValType evaluate(const FeatValType& y_pred, 
+    virtual FeatValType evaluate(const FeatValType& y_pred,
                                  const LabelValType& y_true) const = 0;
-    
-    /** 
+
+    /**
      * compute the derivative of a loss function.
-     * 
+     *
      * @param y_pred FeatValType
      *      The predicted output of a model given an input.
      * @param y_true LabelValType
      *      The true label value for the data point
      * @return A FloatType value representing the derivative of the loss function
     */
-    virtual FeatValType derivate(const FeatValType& y_pred, 
+    virtual FeatValType derivate(const FeatValType& y_pred,
                                  const LabelValType& y_true) const = 0;
 
     /**
      * @brief Sets a parameter for the loss function.
-     * 
+     *
      * @param name The name of the parameter as a string.
      * @param value The value of the parameter as a FloatType.
     */
