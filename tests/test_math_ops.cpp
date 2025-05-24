1b7fd209cf6f75a9d231454bbb94e32479b910c6
qzhao19 <qi.zhao@outlook.fr>
2025-05-24 22:03:21 +0800

    update testcase

--- tests/test_math_ops.cpp
+++ tests/test_math_ops.cpp
@@ -1,14 +1,177 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-#include "common/consts.hpp"
+#include "sgdlib/common/constants.hpp"
 #include "sgdlib/common/prereqs.hpp"
 
 // include private header file
 #include "sgdlib/math/math_ops.hpp"
 
-TEST(ExtmathTest, VectorClipTest){
-    std::vector<double> x = {5.2, 3.3, 1.2, 0.3, 
+
+TEST(MathOpsTest, VecSetTest) {
+    std::vector<double> data(20, 0);
+    const double scalar = 42.0;
+
+    sgdlib::detail::vecset<double>(data, scalar);
+
+    EXPECT_EQ(data.size(), 20);
+    EXPECT_EQ(data[0], scalar);
+    EXPECT_EQ(data.back(), scalar);
+}
+
+
+TEST(MathOpsTest, VeccpyTest) {
+    std::vector<float> x(26, 1.5f), out(26);
+    sgdlib::detail::veccpy<float>(x, out);
+    EXPECT_EQ(out[0], 1.5f);
+}
+
+TEST(MathOpsTest, VecncpyTest) {
+    std::vector<float> x(47, 2.0), out(47);
+    sgdlib::detail::vecncpy<float>(x, out);
+    EXPECT_EQ(out[1], -2.0);
+}
+
+TEST(MathOpsTest, VecclipTest) {
+    std::vector<float> data{0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                            0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                            0.5, 1.5, 2.5, 0.5, 1.5, 2.5};
+    sgdlib::detail::vecclip<float>(data, 1.0, 2.0);
+    EXPECT_LE(data[0], 2.0);
+}
+
+TEST(MathOpsTest, HasinfTest) {
+    std::vector<double> data{1.0, std::numeric_limits<double>::infinity(),
+                            0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                            0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                            0.5, 1.5, 2.5, 0.5, 1.5, 2.5};
+    EXPECT_TRUE(sgdlib::detail::hasinf<double>(data));
+}
+
+TEST(MathOpsTest, Vecnorm2Test) {
+    std::vector<float> x{0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                         0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                         0.5, 1.5, 2.5, 0.5, 1.5, 2.5};
+    EXPECT_NEAR(sgdlib::detail::vecnorm2<float>(x), 7.245688, 1e-5);
+}
+
+TEST(MathOpsTest, Vecnorm2WithPtrTest) {
+    std::vector<float> x{0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                         0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                         0.5, 1.5, 2.5, 0.5, 1.5, 2.5};
+    EXPECT_NEAR(sgdlib::detail::vecnorm2<float>(x.data(), x.data() + x.size()), 7.245688, 1e-5);
+}
+
+TEST(MathOpsTest, Vecnorm1Test) {
+    std::vector<double> x{0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                          0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                          0.5, 1.5, 2.5, 0.5, 1.5, 2.5};
+    EXPECT_DOUBLE_EQ(sgdlib::detail::vecnorm1<double>(x), 27.0);
+}
+
+TEST(MathOpsTest, VecscaleTest) {
+    std::vector<float> x{0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                         0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                         0.5, 1.5, 2.5, 0.5, 1.5, 2.5}, out(18);
+    sgdlib::detail::vecscale<float>(x, 2.0, out);
+    EXPECT_FLOAT_EQ(out[1], 3.0);
+}
+
+TEST(MathOpsTest, VecscalePointersTest) {
+    std::vector<double> data{0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                             0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                             0.5, 1.5, 2.5, 0.5, 1.5, 2.5}, out(18);
+    sgdlib::detail::vecscale<double>(&data[0], &data[18], 3.0, out);
+    // for (auto c : out) {
+    //     std::cout << c << " ";
+    // }
+    // std::cout << std::endl;
+    // std::cout << out[18] << " " << std::endl;
+    EXPECT_DOUBLE_EQ(out[0], 1.5);
+}
+
+TEST(MathOpsTest, VecaddTest) {
+    std::vector<float> a{0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5};
+    std::vector<float> b{0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5}, out(18);
+    sgdlib::detail::vecadd<float>(a, b, out);
+    EXPECT_FLOAT_EQ(out[0], 1.0);
+}
+
+TEST(MathOpsTest, VecaddScaledTest) {
+    std::vector<float> a{1.0, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5};
+    std::vector<float> b{2.0, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5}, out(18);
+    sgdlib::detail::vecadd<float>(a, b, 0.5, out);
+    EXPECT_DOUBLE_EQ(out[0], 2.5);  // 1 + 0.5*2 = 2
+}
+
+TEST(MathOpsTest, VecdiffTest) {
+    std::vector<float> a{4.0, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5};
+    std::vector<float> b{2.0, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5}, out(18);
+    sgdlib::detail::vecdiff<float>(a, b, out);
+    EXPECT_FLOAT_EQ(out[0], 2.0);
+}
+
+TEST(MathOpsTest, VecdiffWithScalarTest) {
+    float scalar = 2.175;
+    std::vector<float> a{4.0, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5};
+    std::vector<float> b{2.0, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5}, out(18);
+    sgdlib::detail::vecdiff<float>(a, b, scalar, out);
+    EXPECT_FLOAT_EQ(out[0], 4.0-2.0*2.175);
+}
+
+
+TEST(MathOpsTest, VecdotTest) {
+    std::vector<float> a{4.0, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5};
+    std::vector<float> b{2.0, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5};
+    EXPECT_DOUBLE_EQ(sgdlib::detail::vecdot<float>(a, b), 60.25);
+}
+
+TEST(MathOpsTest, VecdotWithPtrTest) {
+    std::vector<float> x{4.0, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5};
+    std::vector<float> y{2.0, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5};
+    float result = sgdlib::detail::vecdot<float>(x.data(), x.data() + x.size(), y.data());
+    EXPECT_FLOAT_EQ(result, 60.25f); // 1*4 + 2*5 + 3*6 = 32
+}
+
+
+TEST(MathOpsTest, VecmulTest) {
+    std::vector<float> a{4.0, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5};
+    std::vector<float> b{2.0, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5,
+                        0.5, 1.5, 2.5, 0.5, 1.5, 2.5}, out(18);
+    sgdlib::detail::vecmul<float>(a, b, out);
+    EXPECT_FLOAT_EQ(out[0], 8.0);
+}
+
+
+TEST(MathOpsTest, VectorClipTest){
+    std::vector<double> x = {5.2, 3.3, 1.2, 0.3,
                              4.8, 3.1, 1.6, 0.2};
     sgdlib::detail::clip<double>(x, 2.0, 4.0);
     std::vector<double> expect = {4.0, 3.3, 2.0, 2.0,
@@ -16,13 +179,13 @@ TEST(ExtmathTest, VectorClipTest){
     EXPECT_EQ(x, expect);
 };
 
-TEST(ExtmathTest, ScalarClipTest){
+TEST(MathOpsTest, ScalarClipTest){
     double a = 5.2;
     sgdlib::detail::clip<double>(a, 2.0, 4.0);
     EXPECT_DOUBLE_EQ(a, 4.0);
 };
 
-TEST(ExtmathTest, VectorIsinfTest){
+TEST(MathOpsTest, VectorIsinfTest){
     double a = INF;
     std::vector<double> x1 = {5.2, 3.3, a, 0.3};
     bool has_inf = sgdlib::detail::isinf<double>(x1);
@@ -33,25 +196,25 @@ TEST(ExtmathTest, VectorIsinfTest){
     EXPECT_FALSE(no_inf);
 };
 
-TEST(ExtmathTest, ScalarIsinfTest){
+TEST(MathOpsTest, ScalarIsinfTest){
     double a = INF;
     bool is_inf = sgdlib::detail::isinf<double>(a);
     EXPECT_TRUE(is_inf);
 };
 
-TEST(ExtmathTest, Sqnorm2Test){
+TEST(MathOpsTest, Sqnorm2Test){
     std::vector<double> x = {5.2, 3.3, 2.6, 0.3};
     double norm = sgdlib::detail::sqnorm2<double>(x, true);
     EXPECT_DOUBLE_EQ(norm, 6.691786009728643);
 };
 
-TEST(ExtmathTest, Sqnorm1Test){
+TEST(MathOpsTest, Sqnorm1Test){
     std::vector<double> x = {5.2, 3.3, 2.6, 0.3};
     double norm = sgdlib::detail::norm1<double>(x);
     EXPECT_DOUBLE_EQ(norm, 11.4);
 };
 
-TEST(ExtmathTest, InplaceVectorScalarDotTest){
+TEST(MathOpsTest, InplaceVectorScalarDotTest){
     std::vector<double> x = {5.2, 3.3, 2.6, 0.3};
     double c = 10.0;
     sgdlib::detail::dot<double>(x, c);
@@ -59,7 +222,7 @@ TEST(ExtmathTest, InplaceVectorScalarDotTest){
     EXPECT_EQ(x, expect);
 };
 
-TEST(ExtmathTest, IteratorVectorScalarDotTest){
+TEST(MathOpsTest, IteratorVectorScalarDotTest){
     std::vector<double> x = {5.2, 3.3, 2.6, 0.3};
     std::vector<double> out(x.size());
     double c = 10.0;
@@ -68,7 +231,7 @@ TEST(ExtmathTest, IteratorVectorScalarDotTest){
     EXPECT_EQ(out, expect);
 };
 
-TEST(ExtmathTest, TwoVectorsDotTest){
+TEST(MathOpsTest, TwoVectorsDotTest){
     std::vector<double> x = {5.2, 3.3, 2.6, 0.3};
     std::vector<double> y = {5.2, 3.3, 2.6, 0.3};
     double out = 0.0;
@@ -77,9 +240,9 @@ TEST(ExtmathTest, TwoVectorsDotTest){
     EXPECT_DOUBLE_EQ(out, expect);
 };
 
-TEST(ExtmathTest, RowNormsTest){
-    std::vector<double> x = {5.2, 3.3, 1.2, 0.3, 
-                             6.4, 3.1, 5.5, 1.8, 
+TEST(MathOpsTest, RowNormsTest){
+    std::vector<double> x = {5.2, 3.3, 1.2, 0.3,
+                             6.4, 3.1, 5.5, 1.8,
                              4.75, 3.1, 1.32, 0.1};
     std::vector<double> out(3);
     bool sq = false;
@@ -91,9 +254,9 @@ TEST(ExtmathTest, RowNormsTest){
     }
 };
 
-TEST(ExtmathTest, ColNormsTest){
-    std::vector<double> x = {5.2, 3.3, 1.2, 0.3, 
-                             6.4, 3.1, 5.5, 1.8, 
+TEST(MathOpsTest, ColNormsTest){
+    std::vector<double> x = {5.2, 3.3, 1.2, 0.3,
+                             6.4, 3.1, 5.5, 1.8,
                              4.75, 3.1, 1.32, 0.1};
     std::vector<double> out(4);
     bool sq = true;
