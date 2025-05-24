a25014e41a6a4444ff328515cd45ad964d5c8ae0
qzhao19 <qi.zhao@outlook.fr>
2025-05-24 22:02:00 +0800

    add vecdiff function

--- sgdlib/math/kernels/ops_sse.hpp
+++ sgdlib/math/kernels/ops_sse.hpp
@@ -136,6 +136,16 @@ inline void vecdiff_sse(const T* x, const T* y, std::size_t n, std::size_t m, T*
 };
 
 template <typename T>
+inline void vecdiff_sse(const T* x, const T* y, const T c, std::size_t n, std::size_t m, T* out) {
+    if constexpr (std::is_same_v<T, float>) {
+        vecdiff_sse_float(x, y, c, n, m, out);
+    }
+    else if constexpr (std::is_same_v<T, double>) {
+        vecdiff_sse_double(x, y, c, n, m, out);
+    }
+};
+
+template <typename T>
 inline T vecdot_sse(const T* x, const T* y, std::size_t n, std::size_t m) {
     T prod;
     if constexpr (std::is_same_v<T, float>) {
