6e5b1202d4587e3bbf833efa42e276f467109e89
qzhao19 <qi.zhao@outlook.fr>
2025-06-02 23:29:56 +0800

    cleanup useless testcases

--- tests/test_math_ops.cpp
+++ tests/test_math_ops.cpp
@@ -234,75 +234,16 @@ TEST(MathOpsTest, VecmulTest) {
     EXPECT_FLOAT_EQ(out[0], 8.0);
 }
 
-
-TEST(MathOpsTest, VectorClipTest){
-    std::vector<double> x = {5.2, 3.3, 1.2, 0.3,
-                             4.8, 3.1, 1.6, 0.2};
-    sgdlib::detail::clip<double>(x, 2.0, 4.0);
-    std::vector<double> expect = {4.0, 3.3, 2.0, 2.0,
-                                  4.0, 3.1, 2.0, 2.0};
-    EXPECT_EQ(x, expect);
-};
-
 TEST(MathOpsTest, ScalarClipTest){
     double a = 5.2;
     sgdlib::detail::clip<double>(a, 2.0, 4.0);
     EXPECT_DOUBLE_EQ(a, 4.0);
 };
 
-TEST(MathOpsTest, VectorIsinfTest){
-    double a = INF;
-    std::vector<double> x1 = {5.2, 3.3, a, 0.3};
-    bool has_inf = sgdlib::detail::isinf<double>(x1);
-    EXPECT_TRUE(has_inf);
-
-    std::vector<double> x2 = {5.2, 3.3, 2.6, 0.3};
-    bool no_inf = sgdlib::detail::isinf<double>(x2);
-    EXPECT_FALSE(no_inf);
-};
-
 TEST(MathOpsTest, ScalarIsinfTest){
     double a = INF;
     bool is_inf = sgdlib::detail::isinf<double>(a);
     EXPECT_TRUE(is_inf);
-};
-
-TEST(MathOpsTest, Sqnorm2Test){
-    std::vector<double> x = {5.2, 3.3, 2.6, 0.3};
-    double norm = sgdlib::detail::sqnorm2<double>(x, true);
-    EXPECT_DOUBLE_EQ(norm, 6.691786009728643);
-};
-
-TEST(MathOpsTest, Sqnorm1Test){
-    std::vector<double> x = {5.2, 3.3, 2.6, 0.3};
-    double norm = sgdlib::detail::norm1<double>(x);
-    EXPECT_DOUBLE_EQ(norm, 11.4);
-};
-
-TEST(MathOpsTest, InplaceVectorScalarDotTest){
-    std::vector<double> x = {5.2, 3.3, 2.6, 0.3};
-    double c = 10.0;
-    sgdlib::detail::dot<double>(x, c);
-    std::vector<double> expect = {52.0, 33.0, 26.0, 3.0};
-    EXPECT_EQ(x, expect);
-};
-
-TEST(MathOpsTest, IteratorVectorScalarDotTest){
-    std::vector<double> x = {5.2, 3.3, 2.6, 0.3};
-    std::vector<double> out(x.size());
-    double c = 10.0;
-    sgdlib::detail::dot<double>(x.begin(), x.end(), c, out);
-    std::vector<double> expect = {52.0, 33.0, 26.0, 3.0};
-    EXPECT_EQ(out, expect);
-};
-
-TEST(MathOpsTest, TwoVectorsDotTest){
-    std::vector<double> x = {5.2, 3.3, 2.6, 0.3};
-    std::vector<double> y = {5.2, 3.3, 2.6, 0.3};
-    double out = 0.0;
-    sgdlib::detail::dot<double>(x, y, out);
-    double expect = 44.78;
-    EXPECT_DOUBLE_EQ(out, expect);
 };
 
 TEST(MathOpsTest, RowNormsTest){
