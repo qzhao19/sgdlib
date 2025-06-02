e8e65f694083acf97431081ea0f5a2d0e7eb1187
qzhao19 <qi.zhao@outlook.fr>
2025-06-02 23:29:37 +0800

    cleanup useless codes

--- sgdlib/math/math_ops.hpp
+++ sgdlib/math/math_ops.hpp
@@ -965,31 +965,6 @@ inline T vecaccumul(const T* xbegin,
     return prod;
 };
 
-
-
-/**
- * @brief Clip (limit) the values in a vector.
- *
- * @tparam Type The type of elements in the vector.
- *
- * @param x vector containing elements to clip.
- * @param min, max minimum and maximum value
- *
- * @note The function is marked as inline, which is suitable for small functions
- *       to reduce the overhead of function calls.
-*/
-template<typename Type>
-inline void clip(std::vector<Type>& x, Type min, Type max) {
-    if (min > max) {
-        THROW_INVALID_ERROR("a_min must be less than or equal to a_max.");
-    }
-    std::transform(std::begin(x), std::end(x), std::begin(x),
-        [=] (auto i) {
-            return std::clamp(i, min, max);
-        }
-    );
-};
-
 /**
  * @brief Clips the value to the specified range [min, max]
  *
@@ -1010,26 +985,6 @@ inline void clip(Type& x, Type min, Type max) {
 };
 
 /**
- * @brief check if any element of vector is infinite.
- *
- * @tparam Type The type of elements in the vector.
- *
- * @param x vector containing elements to check infinity.
- *
- * @note The function is marked as inline, which is suitable for small functions
- *       to reduce the overhead of function calls.
-*/
-template<typename Type>
-inline bool isinf(const std::vector<Type>& x) {
-    for (std::size_t i = 0; i < x.size(); ++i) {
-        if (std::isinf(x[i])) {
-            return true;
-        }
-    }
-    return false;
-};
-
-/**
  * @brief check if the given value represents infinity
  *
  * @tparam Type The type of elements for input.
@@ -1046,139 +1001,6 @@ inline bool isinf(const Type& x) {
     }
     return false;
 };
-
-/**
- * @brief calculate the L2 norm of a vector.
- *
- * @tparam Type The type of elements in the vector.
- *
- * @param x a vector of type T.
- * @return The L2 norm of the vector as a custome type.
- *
- * @note The function is marked as inline, which is suitable for small functions
- *       to reduce the overhead of function calls.
- */
-template<typename Type>
-inline Type sqnorm2(const std::vector<Type>& x, bool squared) {
-    Type norm2 = std::inner_product(x.begin(),
-                                    x.end(),
-                                    x.begin(),
-                                    static_cast<Type>(0.0));
-    if (squared) {
-        norm2 = std::sqrt(norm2);
-    }
-
-    return norm2;
-};
-
-/**
- * @brief calculate the L1 norm (Manhattan distance) of a vector.
- *
- * @tparam Type The type of elements in the vector.
- *
- * @param x a vector of type T.
- * @return The L1 norm of the vector as a T.
- *
-*/
-template<typename Type>
-inline Type norm1(const std::vector<Type>& x) {
-    Type norm = 0;
-    for (const Type& value : x) {
-        norm += std::abs(value);
-    }
-    return norm;
-};
-
-/**
- * @brief Applies a scalar multiplication operation to a vector.
- *
- * @tparam Type The type of elements in the vector.
- *
- * @param[in,out] x vector of type T, which will be scaled by the scalar 'c'.
- * @param[in] scalar constant scalar value
- *
- * @note The function is marked as inline, which is suitable for small functions
- *       to reduce the overhead of function calls.
-*/
-template<typename Type>
-inline void dot(std::vector<Type>& x, const Type c) {
-    for (std::size_t i = 0; i < x.size(); ++i) {
-        x[i] *= c;
-    }
-};
-
-/**
- * @brief Applies a scalar multiplication operation to a vector.
- * It computes the sum of the products of all elements within
- * the iterator range from begin to end with a constant c.
- *
- * @tparam Type The type of elements in the vector.
- * @tparam IterType The type of iterator, with a default of std::vector<Type>::const_iterator.
- *
- * @param begin The beginning iterator pointing to the first element
- *      of the vector to calculate the dot product.
- * @param end The ending iterator pointing to the first element following begin.
- * @param c The constant to be multiplied with the elements of the vector.
- * @param out The reference to a vector that stores the result of the dot product.
- *
- * @note The function is marked as inline, which is suitable for small functions
- *       to reduce the overhead of function calls.
- * @note The use of templates and default iterator types allows the function
- *       to be used with different types of vectors and iterators.
- *
-*/
-template<typename Type,
-         typename IterType = typename std::vector<Type>::const_iterator>
-inline void dot(IterType begin, IterType end,
-                const Type c,
-                std::vector<Type>& out) {
-    if(std::distance(begin, end) != out.size()){
-        THROW_INVALID_ERROR("Output vector size is insufficient.");
-    }
-    std::transform(begin, end,
-                   out.begin(),
-                   [c](const Type& elem) {
-                        return elem * c;
-                   });
-};
-
-/**
- * @brief Applies a scalar multiplication operation to a vector.
- * It computes the sum of the products of all elements within
- * the iterator range from begin to end with a constant c.
- *
- * @tparam Type The type of elements in the vector.
- *
- * @param[in] x vector of type T,
- * @param[in] y vector of type T,
- * @param out The reference to a vector that stores the result of the dot product.
- *
- * @note The function is marked as inline, which is suitable for small functions
- *       to reduce the overhead of function calls.
- *
- */
-template <typename Type>
-inline void dot(const std::vector<Type>& x,
-                const std::vector<Type>& y,
-                Type& out) {
-    if (x.size() != y.size()) {
-        THROW_INVALID_ERROR("Vectors must have the same size.");
-    }
-    out = std::inner_product(x.begin(), x.end(), y.begin(), 0.0);
-}
-
-template<typename Type>
-void inline add(const std::vector<Type>& x,
-                const std::vector<Type>& y,
-                std::vector<Type>& out) {
-    // check
-    if (x.size() != y.size()) {
-        THROW_INVALID_ERROR("Vectors must have the same size.");
-    }
-
-    out.resize(x.size());
-    std::transform(x.begin(), x.end(), y.begin(), out.begin(), std::plus<Type>());
-}
 
 /**
  * @brief Computes the row-wise norms of a vector.
@@ -1237,7 +1059,6 @@ void row_norms(const std::vector<Type>& x,
                     });
     }
 };
-
 
 /**
  * @brief Computes the column-wise norms of a vector.
