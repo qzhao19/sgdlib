f8b6b19cd6259bf2703b65bf08b7a0ad35d2741a
qzhao19 <qi.zhao@outlook.fr>
2025-06-04 22:31:19 +0800

    update

--- tests/test_lr_decay.cpp
+++ tests/test_lr_decay.cpp
@@ -3,27 +3,25 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+// include private header file
 #include "sgdlib/core/lr_decay.hpp"
-
-namespace sgdlib {
 
 class LRDecayTest : public ::testing::Test {
 public:
-    virtual void SetUp(std::string lr_decay_policy) {
+    void SetUp(std::string lr_decay_policy) {
         double eta0 = 0.0001, gamma = 0.0025;
         LRDecayParamType lr_decay_param = {{"eta0", eta0}, {"gamma", gamma}};
-        lr_decay = LRDecayRegistry()->Create(lr_decay_policy, lr_decay_param);
+        lr_decay = sgdlib::detail::LRDecayRegistry()->Create(lr_decay_policy, lr_decay_param);
     }
 
-    virtual void TearDown() {}
-    std::unique_ptr<sgdlib::LRDecay> lr_decay; 
+    std::unique_ptr<sgdlib::detail::LRDecay> lr_decay; 
 };
 
 TEST_F(LRDecayTest, InvscalingDecayTest) {
     SetUp("Invscaling");
     double eta = lr_decay->compute(10);
     std::cout << eta << std::endl;
-    EXPECT_DOUBLE_EQ(eta, 9.9426007395295659e-05);
+    EXPECT_DOUBLE_EQ(eta, 9.940231944093156e-05);
 }
 
 TEST_F(LRDecayTest, ExponentialDecayTest) {
@@ -31,6 +29,4 @@ TEST_F(LRDecayTest, ExponentialDecayTest) {
     double eta = lr_decay->compute(10);
     std::cout << eta << std::endl;
     EXPECT_DOUBLE_EQ(eta, 9.7530991202833268e-05);
-}
-
 }
