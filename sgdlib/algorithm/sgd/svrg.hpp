0bf54d5da56fb217ca847733f25a03656080a82c
qzhao19 <qi.zhao@outlook.fr>
2025-06-05 23:55:37 +0800

    refactor svrg optimizer

--- sgdlib/algorithm/sgd/svrg.hpp
+++ sgdlib/algorithm/sgd/svrg.hpp
@@ -73,28 +73,30 @@ public:
                   const std::vector<LabelValType>& y) override {
 
         // Get the number of samples and features from the input data
-        std::size_t num_samples = y.size();
-        std::size_t num_features = this->w0_.size();
-
+        const std::size_t num_samples = y.size();
+        const std::size_t num_features = this->w0_.size();
+        const FeatValType inv_num_samples = 1.0 / static_cast<FeatValType>(num_samples);
         // Initialize iteration counter, sample index, and batch size
         std::size_t iter = 0, sample_index = 0;
-        std::size_t batch_size = num_samples / this->num_inner_;
 
         // Initialize convergence flag and best loss
         bool is_converged = false;
         bool is_infinity = false;
-        FloatType best_loss = INF;
         FeatValType wscale = 1.0;
 
+        const FeatValType eta_avg = this->eta0_ * inv_num_samples;
+        const FeatValType eta_alpha = this->eta0_ * this->alpha_;
+
         //
-        FloatType tmp, fnorm, fnorm_ratio, prev_fnorm;
+        FeatValType reg_dw, fnorm, fnorm_ratio, init_fnorm, alpha_scaled;
 
         // initialize a lookup table for training X, y
-        std::vector<std::size_t> X_data_index(num_samples);
-        std::iota(X_data_index.begin(), X_data_index.end(), 0);
+        this->X_data_index_.resize(num_samples);
+        std::iota(this->X_data_index_.begin(), this->X_data_index_.end(), 0);
+        this->loss_history_.reserve(this->max_iters_ * this->num_inner_);
 
         // initialize gradient,
-        FeatValType y_hat, grad;
+        FeatValType y_hat, grad, loss;
         std::vector<FeatValType> grad_history(num_samples, 0.0);
         std::vector<FeatValType> full_weight_update(num_features, 0.0);
         std::vector<std::size_t> update_history(num_features, 0);
@@ -102,33 +104,36 @@ public:
         // initialize w0 (weight)
         std::vector<FeatValType> w0 = this->w0_;
 
+        // start iteration
         for (iter = 0; iter < this->max_iters_; ++iter) {
-            // compute full gradeint for all data
             // Reset the full weight update vector to zero at the beginning of each iteration
-            std::memset(full_weight_update.data(), 0, full_weight_update.size() * sizeof(FeatValType));
+            sgdlib::detail::vecset<FeatValType>(full_weight_update, 0.0);
 
-            // Compute the full gradient for all samples
+            // compute full gradeint for all data
             for (std::size_t i = 0; i < num_samples; ++i) {
-                y_hat = std::inner_product(&X[i * num_features],
-                                           &X[(i + 1) * num_features],
-                                           w0.begin(), 0.0);
+                y_hat = sgdlib::detail::vecdot<FeatValType>(
+                    X.data() + (i * num_features),
+                    X.data() + ((i + 1) * num_features),
+                    w0.data()
+                );
                 y_hat = y_hat * wscale;
                 grad_history[i] = this->loss_fn_->derivate(y_hat, y[i]);
-                for (std::size_t j = 0; j < num_features; ++j) {
-                    full_weight_update[j] += grad_history[i] * X[i * num_features + j];
-                }
+                sgdlib::detail::vecadd<FeatValType>(
+                    X.data() + (i * num_features),
+                    X.data() + ((i + 1) * num_features),
+                    grad_history[i],
+                    full_weight_update
+                );
             }
 
-            // apply lr decay policy to compute eta
-            const FloatType eta = this->lr_decay_->compute(iter);
-            // start inner loop
+            // inner loop
             for (std::size_t n = 0; n < this->num_inner_; ++n) {
-                // check if we have to shuffle the samples
+                // shuffle the samples
                 if (this->shuffle_) {
-                    sample_index = this->random_state_.sample<std::size_t>(X_data_index);
+                    sample_index = this->random_state_.sample<std::size_t>(this->X_data_index_);
                 }
                 else {
-                    sample_index = n;
+                    sample_index = n % num_samples;
                 }
 
                 // just-in-time update for full weight 1/n * sum(d(f_k_w))
@@ -138,78 +143,81 @@ public:
                 // Update the weights to compensate for the full gradient
                 if (n > 0) {
                     for (std::size_t j = 0; j < num_features; ++j) {
-                        w0[j] += eta / wscale * (n - update_history[j]) * full_weight_update[j];
+                        w0[j] -= eta_avg / wscale * (n - update_history[j]) * full_weight_update[j];
                         update_history[j] = n;
                     }
                 }
 
-                // compute current gradient at sample_index position
-                // y_hat = w0 * X[sample_index : sample_index + num_features]
-                y_hat = std::inner_product(&X[sample_index * num_features],
-                                           &X[(sample_index + 1) * num_features],
-                                           w0.begin(), 0.0);
+                // current gradient at sample_index row data
+                y_hat = sgdlib::detail::vecdot<FeatValType>(
+                    X.data() + (sample_index * num_features),
+                    X.data() + ((sample_index + 1) * num_features),
+                    w0.data()
+                );
                 y_hat = y_hat * wscale;
+                loss = this->loss_fn_->evaluate(y_hat, y[sample_index]);
                 grad = this->loss_fn_->derivate(y_hat, y[sample_index]);
 
-                // Update the weight scale factor based on the learning rate and regularization parameter
-                wscale *= (1.0 - eta * this->alpha_);
+                // update the weight scale factor based on eta*alpha
+                wscale *= (1 - eta_alpha);
 
-                // update w0
-                // grad_history[sample_index] - grad: reduce the variance of gradients
-                for (std::size_t j = 0; j < num_features; ++j) {
-                    w0[j] += eta * (grad_history[sample_index] - grad) / wscale;
-                }
+                // update w: old_gradient - new_gradient
+                sgdlib::detail::vecadd<FeatValType>(
+                    X.data() + (sample_index * num_features),
+                    X.data() + ((sample_index + 1) * num_features),
+                    this->eta0_ * (grad_history[sample_index] - grad) / wscale, w0
+                );
 
-                // possible underflow
-                // Check if the weight scale factor is below the threshold
+                // check if the weight scale factor is below the threshold
+                // rescale the weights and reset the scale factor
                 if (wscale < WSCALE_THRESHOLD) {
-                    // Rescale the weights and reset the scale factor
-                    std::transform(w0.begin(), w0.end(), w0.begin(),
-                                   [wscale](FeatValType val) {
-                                       return val * wscale;
-                                   });
+                    sgdlib::detail::vecscale<FeatValType>(w0, wscale, w0);
                     wscale = 1.0;
                 }
+
+                // store loss value into this->loss_history_
+                this->loss_history_.push_back(loss);
             }
-            // Ensure that full gradient compensation is applied to all un-updated weights
-            // Apply the full gradient compensation to any un-updated weights
+
+            // finalize
             for (std::size_t j = 0; j < num_features; ++j) {
-                w0[j] += eta / wscale * (this->num_inner_ - update_history[j]) * full_weight_update[j];
+                w0[j] -= eta_avg / wscale * (this->num_inner_ - update_history[j]) * full_weight_update[j];
                 update_history[j] = 0;
             }
 
-            // check under/overflow
-            if (sgdlib::detail::isinf<FeatValType>(w0)) {
-                is_infinity = true;
-                break;
-            }
-
-            // convergence test 1
-            // Compute the L2 norm of the loss function
+            // compute the L2 norm of the loss function
             // fnorm = sqrt[(w0 * alpha + 1/n * d(f_k_w))**2]
             fnorm = 0.0;
+            alpha_scaled = this->alpha_ * wscale;
             for (std::size_t j = 0; j < num_features; ++j) {
-                tmp = w0[j] * this->alpha_ + full_weight_update[j] / num_samples;
-                fnorm += tmp * tmp;
+                reg_dw = full_weight_update[j] * inv_num_samples + alpha_scaled * w0[j];
+                fnorm += reg_dw * reg_dw;
             }
             fnorm = std::sqrt(fnorm);
 
             // Store the initial gradient norm
             if (iter == 0) {
-                prev_fnorm = fnorm;
+                init_fnorm = fnorm;
             }
-            // Compute the ratio of the current gradient norm to the previous one
-            fnorm_ratio = fnorm / prev_fnorm;
 
-            if (fnorm_ratio < this->tol_) {
+            // Compute the ratio of the current gradient norm to the previous one
+            fnorm_ratio = fnorm / init_fnorm;
+
+            if (this->verbose_) {
+                PRINT_RUNTIME_INFO(1, "Epoch = ", iter + 1, ", fnorm_ratio = ", fnorm_ratio);
+            }
+
+            if (fnorm_ratio <= this->tol_) {
                 is_converged = true;
                 break;
             }
-            // print info
-            if (this->verbose_) {
-                PRINT_RUNTIME_INFO(2, "Epoch = ", iter + 1,
-                                   ", fnorm_ration = ", fnorm_ratio);
-            }
+        }
+
+        // shrink the this->loss_history_
+        this->loss_history_.shrink_to_fit();
+
+        if (callback_) {
+            callback_(this->loss_history_);
         }
 
         if (is_infinity) {
