9d46069c6633692b8df685d29871373a0f9e2d14
qzhao19 <qi.zhao@outlook.fr>
2025-06-12 23:35:15 +0800

    use ArrayDataset as param at evaluate_with_gradient

--- sgdlib/core/loss/base.hpp
+++ sgdlib/core/loss/base.hpp
@@ -6,24 +6,31 @@
 #include "common/predefs.hpp"
 #include "common/registry.hpp"
 #include "math/math_ops.hpp"
+#include "data/continuous_dataset.hpp"
 
 namespace sgdlib {
 namespace detail {
 
 /**
- * @file base.hpp
- *
- * @class LossFunction
- *
  * @brief Abstract base class representing a loss function
  *
 */
 class LossFunction {
 protected:
+    using CallbackType = std::function<void(const std::vector<FeatValType>&)>;
     LossParamType loss_param_;
+    CallbackType callback_;
+    mutable std::vector<FeatValType> dloss_history_;
+
 public:
-    LossFunction(LossParamType loss_param): loss_param_(loss_param) {};
+    LossFunction(LossParamType loss_param): loss_param_(loss_param),
+        callback_(nullptr) {};
+
     virtual ~LossFunction() = default;
+
+    void set_callback(CallbackType callback) {
+        callback_ = callback;
+    }
 
     /**
      * evaluate the loss value of loss function
@@ -34,8 +41,8 @@ public:
      *      The true label value for the data point
      * @return A FloatType value for loss function value
     */
-    virtual FeatValType evaluate(const FeatValType& y_pred,
-                                 const LabelValType& y_true) const = 0;
+    virtual FeatValType evaluate(const FeatValType &y_pred,
+                                 const LabelValType &y_true) const = 0;
 
     /**
      * compute the derivative of a loss function.
@@ -46,9 +53,8 @@ public:
      *      The true label value for the data point
      * @return A FloatType value representing the derivative of the loss function
     */
-    virtual FeatValType derivate(const FeatValType& y_pred,
-                                 const LabelValType& y_true) const = 0;
-
+    virtual FeatValType derivate(const FeatValType &y_pred,
+                                 const LabelValType &y_true) const = 0;
 
     /**
      * Computes both loss value and gradient vector in a single pass
@@ -60,11 +66,9 @@ public:
      *
      * @return FeatValType Computed loss value
      */
-    virtual FeatValType evaluate_with_gradient(
-        const std::vector<FeatValType>& X,
-        const std::vector<LabelValType>& y,
-        const std::vector<FeatValType>& w,
-        std::vector<FeatValType>& grad) const = 0;
+    virtual FeatValType evaluate_with_gradient(const sgdlib::detail::ArrayDatasetType &dataset,
+                                               const std::vector<FeatValType> &w,
+                                               std::vector<FeatValType> &grad) const = 0;
 
     /**
      * @brief Sets a parameter for the loss function.
@@ -72,7 +76,7 @@ public:
      * @param name The name of the parameter as a string.
      * @param value The value of the parameter as a FloatType.
     */
-    void set_param(const std::string& name, const FloatType& value) {
+    void set_param(const std::string &name, const FloatType &value) {
         loss_param_[name] = value;
     }
 };
