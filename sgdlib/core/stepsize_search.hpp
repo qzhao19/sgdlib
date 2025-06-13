000d06857a08d45eb75c55e937753496df36d4cd
qzhao19 <qi.zhao@outlook.fr>
2025-06-13 23:09:06 +0800

    change included path name

--- sgdlib/core/stepsize_search.hpp
+++ sgdlib/core/stepsize_search.hpp
@@ -2,7 +2,7 @@
 #define CORE_STEPSIZE_SEARCH_HPP_
 
 #include "stepsize_search/constant_search.hpp"
-#include "stepsize_search/basic_line_search.hpp"
+#include "stepsize_search/exact_line_search.hpp"
 #include "stepsize_search/bracketing_line_search.hpp"
 #include "stepsize_search/backtracking_line_search.hpp"
 
