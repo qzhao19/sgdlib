5315d09a41519290e4bd96e53639d82389ab3b77
qzhao19 <qi.zhao@outlook.fr>
2025-06-16 23:25:53 +0800

    update

--- sgdlib/data/continuous_dataset.hpp
+++ sgdlib/data/continuous_dataset.hpp
@@ -12,9 +12,9 @@ namespace detail {
 class ArrayDataset {
 private:
     // define unique_ptr to manager row cache
-    std::unique_ptr<FeatValType[]> X_row_cache_;
-    std::shared_ptr<const FeatValType[]> X_data_ptr_;
-    std::shared_ptr<const LabelValType[]> y_data_ptr_;
+    std::unique_ptr<sgdlib::FeatureScalarType[]> X_row_cache_;
+    std::shared_ptr<const sgdlib::FeatureScalarType[]> X_data_ptr_;
+    std::shared_ptr<const sgdlib::LabelScalarType[]> y_data_ptr_;
     std::size_t nrows_, ncols_;
     bool enable_cache_;
 
@@ -23,7 +23,7 @@ private:
         #pragma omp parallel for
         #endif
         for (std::size_t r = 0; r < nrows_; ++r) {
-            FeatValType* x_row_ptr = X_row_cache_.get() + r * ncols_;
+            sgdlib::FeatureScalarType* x_row_ptr = X_row_cache_.get() + r * ncols_;
             for (std::size_t c = 0; c < ncols_; ++c) {
                 x_row_ptr[c] = X_data_ptr_[r + c * nrows_];
             }
@@ -32,8 +32,8 @@ private:
 
 public:
     // constructor for raw ptr
-    ArrayDataset(std::shared_ptr<const FeatValType[]> X_data_ptr,
-        std::shared_ptr<const LabelValType[]> y_data_ptr,
+    ArrayDataset(std::shared_ptr<const sgdlib::FeatureScalarType[]> X_data_ptr,
+        std::shared_ptr<const sgdlib::LabelScalarType[]> y_data_ptr,
         std::size_t nrows,
         std::size_t ncols,
         bool enable_cache = true):
@@ -48,18 +48,18 @@ public:
         }
 
         if (enable_cache_) {
-            X_row_cache_ = std::make_unique<FeatValType[]>(nrows * ncols);
+            X_row_cache_ = std::make_unique<sgdlib::FeatureScalarType[]>(nrows * ncols);
             col2row_cache();
         }
     };
 
-    ArrayDataset(const std::vector<FeatValType>& X_data,
-        const std::vector<LabelValType>& y_data,
+    ArrayDataset(const std::vector<sgdlib::FeatureScalarType>& X_data,
+        const std::vector<sgdlib::LabelScalarType>& y_data,
         std::size_t nrows,
         std::size_t ncols,
         bool enable_cache = true) : ArrayDataset(
-            std::shared_ptr<const FeatValType[]>(X_data.data(), [](auto ptr){}),
-            std::shared_ptr<const LabelValType[]>(y_data.data(), [](auto ptr){}),
+            std::shared_ptr<const sgdlib::FeatureScalarType[]>(X_data.data(), [](auto ptr){}),
+            std::shared_ptr<const sgdlib::LabelScalarType[]>(y_data.data(), [](auto ptr){}),
             nrows,
             ncols,
             enable_cache) {
@@ -78,39 +78,39 @@ public:
         ncols_(other.ncols_),
         enable_cache_(other.enable_cache_) {
             // deep copy X data
-            auto X_data_buffer = std::make_unique<FeatValType[]>(nrows_ * ncols_);
+            auto X_data_buffer = std::make_unique<sgdlib::FeatureScalarType[]>(nrows_ * ncols_);
             std::memcpy(X_data_buffer.get(),
                 other.X_data_ptr_.get(),
-                nrows_ * ncols_ * sizeof(FeatValType)
+                nrows_ * ncols_ * sizeof(sgdlib::FeatureScalarType)
             );
-            X_data_ptr_ = std::shared_ptr<const FeatValType[]>(X_data_buffer.release());
+            X_data_ptr_ = std::shared_ptr<const sgdlib::FeatureScalarType[]>(X_data_buffer.release());
             // deep copy y data
-            auto y_data_buffer = std::make_unique<LabelValType[]>(nrows_);
+            auto y_data_buffer = std::make_unique<sgdlib::LabelScalarType[]>(nrows_);
             std::memcpy(y_data_buffer.get(),
                 other.y_data_ptr_.get(),
-                nrows_ * sizeof(LabelValType)
+                nrows_ * sizeof(sgdlib::LabelScalarType)
             );
-            y_data_ptr_ = std::shared_ptr<const LabelValType[]>(y_data_buffer.release());
+            y_data_ptr_ = std::shared_ptr<const sgdlib::LabelScalarType[]>(y_data_buffer.release());
 
             if (enable_cache_) {
-                X_row_cache_ = std::make_unique<FeatValType[]>(nrows_ * ncols_);
+                X_row_cache_ = std::make_unique<sgdlib::FeatureScalarType[]>(nrows_ * ncols_);
                 std::memcpy(X_row_cache_.get(),
                     other.X_row_cache_.get(),
-                    nrows_ * ncols_ * sizeof(FeatValType)
+                    nrows_ * ncols_ * sizeof(sgdlib::FeatureScalarType)
                 );
             }
     };
 
     ~ArrayDataset() = default;
 
-    void X_row_data(const std::size_t i, std::vector<FeatValType>& row) const {
+    void X_row_data(const std::size_t i, std::vector<sgdlib::FeatureScalarType>& row) const {
         if (i >= nrows_) {
             THROW_OUT_RANGE_ERROR("Row index out of range");
         }
         if (enable_cache_) {
             std::memcpy(row.data(),
                 X_row_cache_.get() + i * ncols_,
-                ncols_ * sizeof(FeatValType));
+                ncols_ * sizeof(sgdlib::FeatureScalarType));
         }
         else {
             // collect data from col-major-order X
@@ -120,32 +120,32 @@ public:
         }
     }
 
-    void y_row_data(const std::size_t i, LabelValType& row) const {
+    void y_row_data(const std::size_t i, sgdlib::LabelScalarType& row) const {
         if (i >= nrows_) {
             THROW_OUT_RANGE_ERROR("Row index out of range");
         }
         row = y_data_ptr_[i];
     }
 
-    void X_column_data(const std::size_t j, std::vector<FeatValType>& column) const {
+    void X_column_data(const std::size_t j, std::vector<sgdlib::FeatureScalarType>& column) const {
         if (j >= ncols_) {
             THROW_OUT_RANGE_ERROR("Column index out of range");
         }
         std::memcpy(column.data(),
             X_data_ptr_.get() + j * nrows_,
-            nrows_ * sizeof(FeatValType));
+            nrows_ * sizeof(sgdlib::FeatureScalarType));
     }
 
-    void y_column_data(std::vector<LabelValType>& column) const {
-        std::memcpy(column.data(), y_data_ptr_.get(), nrows_ * sizeof(LabelValType));
+    void y_column_data(std::vector<sgdlib::LabelScalarType>& column) const {
+        std::memcpy(column.data(), y_data_ptr_.get(), nrows_ * sizeof(sgdlib::LabelScalarType));
     }
 
     // read-only
-    const FeatValType* X_data_ptr() const { return X_data_ptr_.get(); }
-    const LabelValType* y_data_ptr() const { return y_data_ptr_.get(); }
+    const sgdlib::FeatureScalarType* X_data_ptr() const { return X_data_ptr_.get(); }
+    const sgdlib::LabelScalarType* y_data_ptr() const { return y_data_ptr_.get(); }
 
     // read-only row cache
-    const FeatValType* Xt_data_ptr() const { return X_row_cache_.get(); }
+    const sgdlib::FeatureScalarType* Xt_data_ptr() const { return X_row_cache_.get(); }
 
     std::size_t nrows() const { return nrows_; }
     std::size_t ncols() const { return ncols_; }
@@ -153,10 +153,11 @@ public:
 
 };
 
-//
-using ArrayDatasetType = ArrayDataset;
+} // namespace detail
 
-}
+// expose on sgdlib namespac
+using ArrayDatasetType = sgdlib::detail::ArrayDataset;
+
 }
 
 #endif // DATA_CONTINUOUS_DATASET_HPP_
