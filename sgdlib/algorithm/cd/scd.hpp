95d9db167f4133a2c922b63027dce514f70cd5a4
qzhao19 <qi.zhao@outlook.fr>
2025-06-21 22:43:05 +0800

    update

--- sgdlib/algorithm/cd/scd.hpp
+++ sgdlib/algorithm/cd/scd.hpp
@@ -5,98 +5,58 @@
 
 namespace sgdlib {
 
-/**
- * @file scd.hpp
- *
- * @class SCD
- *
- * @brief Implements the Stochastic Coordinate Descent (SCD) optimization algorithm.
- *
- * This class inherits from `BaseOptimizer` and provides functionality for optimizing
- * machine learning models using the Stochastic Coordinate Descent (SCD) algorithm.
- * SCD optimizes the model by updating one coordinate (feature weight) at a time,
- * making it efficient for high-dimensional problems.
- *
-*/
 class SCD: public BaseOptimizer {
 public:
-    /**
-     * @brief Constructor for the SCD optimizer.
-     *
-     * Initializes the SCD optimizer with the given parameters and passes them to the
-     * base class `BaseOptimizer`.
-     *
-     * @param w0 Initial weight vector for the model.
-     * @param loss The loss function to be minimized.
-     * @param alpha L1 regularization parameter.
-     * @param tol Tolerance for convergence.
-     * @param max_iters Maximum number of iterations for optimization.
-     * @param random_seed Seed for the random number generator.
-     * @param shuffle If true, shuffles the data before each epoch (default: true).
-     * @param verbose If true, enables logging of optimization progress (default: true).
-     *
-     * @note This constructor calls the constructor of the base class `BaseOptimizer` to
-     *       complete the initialization of the optimizer.
-     * @see BaseOptimizer
-     */
-    SCD(const std::vector<FeatValType>& w0,
+    SCD(const std::vector<sgdlib::FeatureScalarType>& w0,
         std::string loss,
-        FloatType alpha,
-        FloatType tol,
+        sgdlib::ScalarType alpha,
+        sgdlib::ScalarType tol,
         std::size_t max_iters,
         std::size_t random_seed,
         bool shuffle = true,
-        bool verbose = true): BaseOptimizer(w0,
-            loss,
-            alpha,
-            tol,
-            max_iters,
-            random_seed,
-            shuffle,
-            verbose) {};
+        bool verbose = true): Optimizer(w0, loss, tol, max_iters, verbose) {
+            this->alpha_ = alpha;
+            this->random_seed_ = random_seed;
+            this->shuffle_ = shuffle;
+    }
 
-    /**
-     * @brief Destructor for the SCD optimizer.
-     *
-     * Default destructor.
-    */
     ~SCD() = default;
 
-    void optimize(const std::vector<FeatValType>& X,
-                  const std::vector<LabelValType>& y) override {
-
-        const std::size_t num_samples = y.size();
-        const std::size_t num_features = this->w0_.size();
-        const FeatValType inv_num_samples = 1.0 / static_cast<FeatValType>(num_samples);
+    void optimize(const sgdlib::ArrayDatasetType& dataset) override {
+        const std::size_t num_samples = dataset.nrows();
+        const std::size_t num_features = dataset.ncols();
+        const sgdlib::FeatureScalarType inv_num_samples = 1.0 / static_cast<sgdlib::FeatureScalarType>(num_samples);
 
         // initialize w0 (weight)
-        std::vector<FeatValType> w0 = this->w0_;
+        std::vector<sgdlib::FeatureScalarType> w0 = this->w0_;
 
         // initialize loss, loss_history, weight_update,
         this->loss_history_.reserve(this->max_iters_);
-        std::vector<FeatValType> y_hat(num_samples, 0.0);
-        FeatValType weight_update, grad, loss, dloss;
-
-        // FeatValType y_hat;
+        std::vector<sgdlib::FeatureScalarType> y_hat(num_samples);
+        sgdlib::FeatureScalarType weight_update, grad, loss, dloss;
+        sgdlib::FeatureScalarType max_weight, max_weight_update;
+        sgdlib::FeatureScalarType prev_weight;
         bool is_converged = false;
-        FeatValType prev_weight;
+
+        // init x_j, y_i
+        std::vector<sgdlib::LabelScalarType> y;
+        dataset.y_column_data(y);
+        std::vector<sgdlib::FeatureScalarType> x_col(num_samples);
 
         std::size_t iter = 0;
         std::size_t feature_index;
 
         // compute column-wise norm2
-        std::vector<FeatValType> X_col_norm(num_features);
-        sgdlib::detail::col_norms<FeatValType>(X, false, X_col_norm);
+        std::vector<sgdlib::FeatureScalarType> X_col_norm(num_features);
+        sgdlib::detail::col_norms<sgdlib::FeatureScalarType>(dataset, false, X_col_norm);
 
-        FeatValType max_weight, max_weight_update;
         for (iter = 0; iter < this->max_iters_; ++iter) {
-
             max_weight = 0.0;
             max_weight_update = 0.0;
             std::size_t best_feature_index;
-            FeatValType best_weight_update;
-            FeatValType best_descent = -1.0;
-            FeatValType pred_descent;
+            sgdlib::FeatureScalarType best_weight_update;
+            sgdlib::FeatureScalarType best_descent = -1.0;
+            sgdlib::FeatureScalarType pred_descent;
 
             // cycle through all the features
             for (feature_index = 0; feature_index < num_features; ++feature_index) {
@@ -112,11 +72,12 @@ public:
 
                 // compute gradient for target feature X[:, feature_index]
                 dloss = 0.0;
-#if defined(USE_OPENMP)
+                #if defined(USE_OPENMP)
                 #pragma omp parallel for reduction(+:dloss)
-#endif
+                #endif
+                dataset.X_column_data(feature_index, x_col);
                 for (std::size_t i = 0; i < num_samples; ++i) {
-                    dloss += this->loss_fn_->derivate(y_hat[i], y[i]) * X[i * num_features + feature_index];
+                    dloss += this->loss_fn_->derivate(y_hat[i], y[i]) * x_col[i];
                 }
                 grad = dloss * inv_num_samples;
 
@@ -164,22 +125,16 @@ public:
             max_weight_update = std::fmax(max_weight_update, std::abs(w0[feature_index] - prev_weight));
 
             // update inner product y_hat
-#if defined(USE_OPENMP)
+            #if defined(USE_OPENMP)
             #pragma omp parallel for
-#endif
-            for (std::size_t i = 0; i < num_samples; ++i) {
-                y_hat[i] += weight_update * X[i * num_features + feature_index];
-            }
+            #endif
+            sgdlib::detail::vecadd(x_col, weight_update, y_hat);
 
             // compute loss
             loss = 0.0;
-#if defined(USE_OPENMP)
-            #pragma omp parallel for reduction(+:loss)
-#endif
             for (std::size_t i = 0; i < num_samples; ++i) {
                 loss += this->loss_fn_->evaluate(y_hat[i], y[i]);
             }
-
             loss *= inv_num_samples;
             // store loss value into loss_history
             this->loss_history_.push_back(loss);
@@ -187,8 +142,7 @@ public:
             // print info
             if (this->verbose_) {
                 PRINT_RUNTIME_INFO(5, "Epoch = ", iter + 1,
-                                   ", wnorm1 = ", sgdlib::detail::vecnorm1<FeatValType>(w0) ,
-                                   ", loss = ", loss);
+                                   ", wnorm1 = ", sgdlib::detail::vecnorm1<sgdlib::FeatureScalarType>(w0));
             }
 
             // convergence check maximum coordinate update
@@ -206,7 +160,7 @@ public:
         // shrink the loss_history
         this->loss_history_.shrink_to_fit();
 
-        // call callback function
+        // // call callback function
         if (callback_) {
             callback_(this->loss_history_);
         }
@@ -218,7 +172,7 @@ public:
         this->w_opt_ = w0;
     }
 
-    const FeatValType get_intercept() const override {
+    const sgdlib::FeatureScalarType get_intercept() const override {
         THROW_LOGIC_ERROR("The 'get_intercept' method is not supported for this SCD optimizer.");
         return 0.0;
     }
