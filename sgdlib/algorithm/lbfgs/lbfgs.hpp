e9b0b433677b6d8f298d236d68b726f9a04c00e3
qzhao19 <qi.zhao@outlook.fr>
2025-06-29 23:20:56 +0800

    update

--- sgdlib/algorithm/lbfgs/lbfgs.hpp
+++ sgdlib/algorithm/lbfgs/lbfgs.hpp
@@ -5,103 +5,64 @@
 
 namespace sgdlib {
 
-/**
- * @file lbfgs.hpp
- *
- * @class LBFGS
- *
- * @brief Implements the Limited-memory Broyden–Fletcher–Goldfarb–Shanno (L-BFGS)
- * optimization algorithm.
- *
- * This class inherits from `BaseOptimizer` and provides functionality for optimizing
- * machine learning models using the L-BFGS algorithm. L-BFGS is a quasi-Newton method
- * that approximates the Broyden–Fletcher–Goldfarb–Shanno (BFGS) algorithm using a limited
- * amount of memory, making it suitable for large-scale optimization problems.
- */
 class LBFGS: public BaseOptimizer {
 private:
     struct LimitedMemoryData {
-        FeatValType mem_ys;
-        FeatValType mem_alpha;
-        std::vector<FeatValType> mem_y;
-        std::vector<FeatValType> mem_s;
+        sgdlib::FeatureScalarType mem_ys;
+        sgdlib::FeatureScalarType mem_alpha;
+        std::vector<sgdlib::FeatureScalarType> mem_y;
+        std::vector<sgdlib::FeatureScalarType> mem_s;
     };
-
     using LimitedMemoryDataType = LimitedMemoryData;
 
 public:
-    /**
-     * @brief Constructor for the LBFGS optimizer.
-     *
-     * Initializes the LBFGS optimizer with the given parameters and passes them to the
-     * base class `BaseOptimizer`.
-     *
-     * @param w0 Initial weight vector for the model.
-     * @param loss Name of the loss function, of type `std::string`.
-     *             Supported loss functions include "LogLoss", "hinge_loss", etc.
-     * @param search_policy Name of the search policy for selecting search directions during optimization.
-     *                      Supported policies include "ARMIJO", "WOLFE", etc.
-     * @param delta The parameter for convergence test, it determines the minimum rate of
-     *              decrease of theobjective function
-     * @param tol Tolerance for convergence. Optimization stops when the change
-     *            in the objective function value is less than this threshold.
-     * @param max_iters Maximum number of iterations for optimization.
-     * @param mem_size Memory size，the number of corrections to approximate the inverse hessian matrix.
-     * @param past Number of past iterations to consider for estimation.
-     * @param stepsize_search_params Step size search parameters, of type `StepSizeSearchParamType*`.
-     *                               Contains specific parameters for step size search (e.g. dec_factor).
-     * @param verbose If true, enables logging of optimization progress (default: true).
-     *
-     * @note This constructor calls the constructor of the base class `BaseOptimizer` to
-     *       complete the initialization of the optimizer.
-     * @see BaseOptimizer
-     */
-    LBFGS(const std::vector<FeatValType>& w0,
+    LBFGS(const std::vector<sgdlib::FeatureScalarType>& w0,
           std::string loss,
           std::string search_policy,
-          FloatType delta,
-          FloatType tol,
+          sgdlib::ScalarType delta,
+          sgdlib::ScalarType tol,
           std::size_t max_iters,
           std::size_t mem_size,
           std::size_t past,
           StepSizeSearchParamType* stepsize_search_params,
-          bool verbose = true): BaseOptimizer(w0,
-            loss,
-            search_policy,
-            delta,
-            tol,
-            max_iters,
-            mem_size,
-            past,
-            stepsize_search_params,
-            verbose) {};
+          bool verbose = true): Optimizer(w0, loss, tol, max_iters, verbose) {
+        this->search_policy_ = search_policy;
+        this->delta_ = delta;
+        this->mem_size_ = mem_size;
+        this->past_ = past;
+        if (stepsize_search_params == nullptr) {
+            stepsize_search_params_ = &sgdlib::detail::DEFAULT_STEPSIZE_SEARCH_PARAMS;
+            PRINT_RUNTIME_INFO(1, "Use default search parameters.");
+        }
+        this->stepsize_search_params_ = stepsize_search_params;
+        init_loss_params();
+    };
 
     ~LBFGS() = default;
 
-    void optimize(const std::vector<FeatValType>& X,
-                  const std::vector<LabelValType>& y) override {
+    void optimize(const sgdlib::ArrayDatasetType& dataset) override {
 
-        std::size_t num_samples = y.size();
-        std::size_t num_features = this->w0_.size();
+        const std::size_t num_samples = dataset.nrows();
+        const std::size_t num_features = dataset.ncols();
+        const sgdlib::FeatureScalarType inv_num_samples =1.0 / static_cast<sgdlib::FeatureScalarType>(num_samples);
 
         // initialize w0 (weight)
-        std::vector<FeatValType> x = this->w0_;
+        std::vector<sgdlib::FeatureScalarType> x = this->w0_;
 
         // define the initial parameters
-        FeatValType y_hat;
+        sgdlib::FeatureScalarType y_hat;
         std::size_t i, j, k, end, bound;
-        FeatValType rate, beta = 0.0;
-        FeatValType fx = 0.0, ys = 0.0, yy = 0.0;
+        sgdlib::FeatureScalarType rate, beta = 0.0;
+        sgdlib::FeatureScalarType fx = 0.0, ys = 0.0, yy = 0.0;
 
         // init the loss history vector
-        // std::vector<FeatValType> loss_history;
         this->loss_history_.reserve(num_samples * this->max_iters_);
 
         // intermediate variables: previous x, gradient, previous gradient, directions
-        std::vector<FeatValType> xp(num_features);
-        std::vector<FeatValType> g(num_features);
-        std::vector<FeatValType> gp(num_features);
-        std::vector<FeatValType> d(num_features);
+        std::vector<sgdlib::FeatureScalarType> xp(num_features);
+        std::vector<sgdlib::FeatureScalarType> g(num_features);
+        std::vector<sgdlib::FeatureScalarType> gp(num_features);
+        std::vector<sgdlib::FeatureScalarType> d(num_features);
 
         // initialize the limited memory data
         // mem_s: storing changes of parameters in the past
@@ -116,18 +77,18 @@ public:
         }
 
         // vector for storing previous values of the objective function
-        std::vector<FeatValType> pfx(std::max(static_cast<std::size_t>(1), this->past_));
+        std::vector<sgdlib::FeatureScalarType> pfx(std::max(1, this->past_));
 
         // call step search policy
-        std::unique_ptr<sgdlib::detail::StepSizeSearch<sgdlib::detail::LossFunction>> stepsize_search;
+        std::unique_ptr<sgdlib::detail::StepSizeSearch> stepsize_search;
         if (this->search_policy_ == "BacktrackingLineSearch") {
-            stepsize_search = std::make_unique<sgdlib::detail::BacktrackingLineSearch<sgdlib::detail::LossFunction>>(
-                X, y, this->loss_fn_, this->stepsize_search_params_
+            stepsize_search = std::make_unique<sgdlib::detail::BacktrackingLineSearch>(
+                dataset, this->loss_fn_, this->stepsize_search_params_
             );
         }
         else if (this->search_policy_ == "BracketingLineSearch") {
-            stepsize_search = std::make_unique<sgdlib::detail::BracketingLineSearch<sgdlib::detail::LossFunction>>(
-                X, y, this->loss_fn_, this->stepsize_search_params_
+            stepsize_search = std::make_unique<sgdlib::detail::BracketingLineSearch>(
+                dataset, this->loss_fn_, this->stepsize_search_params_
             );
         }
         else {
@@ -135,8 +96,9 @@ public:
         }
 
         // compute intial loss value and gradeint with full data X
-        fx = this->loss_fn_->evaluate_with_gradient(X, y, x, g);
-
+        fx = this->loss_fn_->evaluate_with_gradient(dataset, x, g);
+        fx *= inv_num_samples;
+        sgdlib::detail::vecscale<sgdlib::FeatureScalarType>(g, inv_num_samples, g);
         // store the initial value of the cost function fx
         pfx[0] = fx;
 
@@ -146,8 +108,8 @@ public:
         }
 
         // compute norm2 of g and d to make sure initial vars are not stationary point
-        FeatValType xnorm = sgdlib::detail::vecnorm2<FeatValType>(x, false);
-        FeatValType gnorm = sgdlib::detail::vecnorm2<FeatValType>(g, false);
+        sgdlib::FeatureScalarType xnorm = sgdlib::detail::vecnorm2<sgdlib::FeatureScalarType>(x, false);
+        sgdlib::FeatureScalarType gnorm = sgdlib::detail::vecnorm2<sgdlib::FeatureScalarType>(g, false);
 
         // Convergence test 0 -- gradient
         // make sure that the initial variables are not a minimizer
@@ -157,7 +119,7 @@ public:
         }
 
         // intial step size stepsize = 1.0 / norm2(d)
-        FeatValType stepsize = 1.0 / sgdlib::detail::vecnorm2<FeatValType>(d, false);
+        sgdlib::FeatureScalarType stepsize = 1.0 / sgdlib::detail::vecnorm2<sgdlib::FeatureScalarType>(d, false);
 
         k = 1;
         end = 0;
@@ -182,8 +144,8 @@ public:
             // Convergence test 1 -- gradient test
             // criterion is given by the following formula:
             // ||g(x)|| / max(1, ||x||) < tol
-            xnorm = sgdlib::detail::vecnorm2<FeatValType>(x, false);
-            gnorm = sgdlib::detail::vecnorm2<FeatValType>(g, false);
+            xnorm = sgdlib::detail::vecnorm2<sgdlib::FeatureScalarType>(x, false);
+            gnorm = sgdlib::detail::vecnorm2<sgdlib::FeatureScalarType>(g, false);
 
             if (this->verbose_) {
                 PRINT_RUNTIME_INFO(1, "iteration = ", k,
@@ -216,19 +178,19 @@ public:
             // Update vectors s and y:
             // s_{k+1} = x_{k+1} - x_{k} = step * d_{k}.
             // y_{k+1} = g_{k+1} - g_{k}.
-            sgdlib::detail::vecdiff<FeatValType>(x, xp, limited_mem_data[end].mem_s);
-            sgdlib::detail::vecdiff<FeatValType>(g, gp, limited_mem_data[end].mem_y);
+            sgdlib::detail::vecdiff<sgdlib::FeatureScalarType>(x, xp, limited_mem_data[end].mem_s);
+            sgdlib::detail::vecdiff<sgdlib::FeatureScalarType>(g, gp, limited_mem_data[end].mem_y);
 
             // Compute scalars ys and yy:
             // ys = y^t @ s, s = 1 / rho.
             // yy = y^t @ y.
             // Notice that yy is used for scaling the hessian matrix H_0 (Cholesky factor).
-            ys = sgdlib::detail::vecdot<FeatValType>(limited_mem_data[end].mem_y, limited_mem_data[end].mem_s);
-            yy = sgdlib::detail::vecdot<FeatValType>(limited_mem_data[end].mem_y, limited_mem_data[end].mem_y);
+            ys = sgdlib::detail::vecdot<sgdlib::FeatureScalarType>(limited_mem_data[end].mem_y, limited_mem_data[end].mem_s);
+            yy = sgdlib::detail::vecdot<sgdlib::FeatureScalarType>(limited_mem_data[end].mem_y, limited_mem_data[end].mem_y);
             limited_mem_data[end].mem_ys = ys;
 
             // compute negative of gradient: d = -g
-            sgdlib::detail::vecncpy<FeatValType>(g, d);
+            sgdlib::detail::vecncpy<sgdlib::FeatureScalarType>(g, d);
 
             // bound: number of currently available historical messages
             // k: number of iterations
@@ -247,32 +209,32 @@ public:
                 j = (j + this->mem_size_ - 1) % this->mem_size_;
 
                 // alpha_{j} = s^{T}_{j} @ d_{j} * rho_{j}, rho_{j} = 1/mem_ys
-                limited_mem_data[j].mem_alpha = sgdlib::detail::vecdot<FeatValType>(
+                limited_mem_data[j].mem_alpha = sgdlib::detail::vecdot<sgdlib::FeatureScalarType>(
                     limited_mem_data[j].mem_s, d
                 );
                 limited_mem_data[j].mem_alpha /= limited_mem_data[j].mem_ys;
 
                 // update d_{i} = d_{i+1} - (alpha_{i} * y_{i})
-                sgdlib::detail::vecadd<FeatValType>(
+                sgdlib::detail::vecadd<sgdlib::FeatureScalarType>(
                     limited_mem_data[j].mem_y,
                     -limited_mem_data[j].mem_alpha, d
                 );
             }
 
             // scale Hessian H_0, dirctyion *= scale_ceoff;
-            const FeatValType scale_ceoff = ys / yy;
-            sgdlib::detail::vecscale<FeatValType>(d, scale_ceoff, d);
+            const sgdlib::FeatureScalarType scale_ceoff = ys / yy;
+            sgdlib::detail::vecscale<sgdlib::FeatureScalarType>(d, scale_ceoff, d);
 
             // loop2: backwards recursion
             for (i = 0; i < bound; ++i) {
                 // compute beta_j = rho_{j} * y_{T}_{j} @ d_{J}, rho_{j} = 1/mem_ys
-                beta = sgdlib::detail::vecdot<FeatValType>(
+                beta = sgdlib::detail::vecdot<sgdlib::FeatureScalarType>(
                     limited_mem_data[j].mem_y, d
                 );
                 beta /= limited_mem_data[j].mem_ys;
 
                 // update gamm_{i+1} = gamm_{i} + (alpha_{j} - beta_{j}) * s_{j}
-                sgdlib::detail::vecadd<FeatValType>(
+                sgdlib::detail::vecadd<sgdlib::FeatureScalarType>(
                     limited_mem_data[j].mem_s,
                     limited_mem_data[j].mem_alpha - beta,
                     d
@@ -295,7 +257,7 @@ public:
         this->w_opt_ = x;
     };
 
-    const FeatValType get_intercept() const override {
+    const sgdlib::FeatureScalarType get_intercept() const override {
         THROW_LOGIC_ERROR("The 'get_intercept' method is not supported for this LBFGS optimizer.");
         return 0.0;
     }
